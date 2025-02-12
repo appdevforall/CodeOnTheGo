0ec7e2be4b7133b3f512e24372cfbd173272ad82
Egor Hutsko <egorka.gutko@gmail.com>
2025-02-12 14:17:04 +0100

    [ADFA-365] - project build test with Bottom navigation Drawer Project

--- app/src/androidTest/kotlin/com/itsaky/androidide/ProjectBuildTest.kt
+++ app/src/androidTest/kotlin/com/itsaky/androidide/ProjectBuildTest.kt
@@ -4,11 +4,13 @@ import androidx.test.ext.junit.rules.activityScenarioRule
 import com.itsaky.androidide.activities.SplashActivity
 import com.itsaky.androidide.scenarios.InitializationProjectAndCancelingBuildScenario
 import com.itsaky.androidide.scenarios.NavigateToMainScreenScenario
-import com.itsaky.androidide.screens.HomeScreen.clickCreateProject
+import com.itsaky.androidide.screens.HomeScreen.clickCreateProjectHomeScreen
+import com.itsaky.androidide.screens.ProjectSettingsScreen.clickCreateProjectProjectSettings
 import com.itsaky.androidide.screens.ProjectSettingsScreen.selectJavaLanguage
 import com.itsaky.androidide.screens.ProjectSettingsScreen.selectKotlinLanguage
 import com.itsaky.androidide.screens.TemplateScreen.selectTemplate
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import com.kaspersky.kaspresso.testcases.core.testcontext.TestContext
 import org.junit.Rule
 import org.junit.Test
 
@@ -20,68 +22,96 @@ class ProjectBuildTest : TestCase() {
     @Test
     fun test_projectBuild_baseProject_java() {
         run {
-            step("Initialize the app") {
-                scenario(NavigateToMainScreenScenario())
-            }
-            clickCreateProject()
+            navigateToMainScreen()
+            clickCreateProjectHomeScreen()
             step("Select the basic project") {
                 selectTemplate(R.string.template_basic)
             }
             selectJavaLanguage()
-            step("Initialization the project and cancelling the build") {
-                scenario(InitializationProjectAndCancelingBuildScenario())
-            }
+            clickCreateProjectProjectSettings()
+            initializeProjectAndCancelBuild()
         }
     }
 
     @Test
     fun test_projectBuild_baseProject_kotlin() {
         run {
-            step("Initialize the app") {
-                scenario(NavigateToMainScreenScenario())
-            }
-            clickCreateProject()
+            navigateToMainScreen()
+            clickCreateProjectHomeScreen()
             step("Select the basic project") {
                 selectTemplate(R.string.template_basic)
             }
             selectKotlinLanguage()
-            step("Initialization the project and cancelling the build") {
-                scenario(InitializationProjectAndCancelingBuildScenario())
-            }
+            clickCreateProjectProjectSettings()
+            initializeProjectAndCancelBuild()
         }
     }
 
     @Test
     fun test_projectBuild_navigationDrawerProject_java() {
         run {
-            step("Initialize the app") {
-                scenario(NavigateToMainScreenScenario())
-            }
-            clickCreateProject()
+            navigateToMainScreen()
+            clickCreateProjectHomeScreen()
             step("Select the navigation drawer project") {
                 selectTemplate(R.string.template_navigation_drawer)
             }
             selectJavaLanguage()
-            step("Initialization the project and cancelling the build") {
-                scenario(InitializationProjectAndCancelingBuildScenario())
-            }
+            clickCreateProjectProjectSettings()
+            initializeProjectAndCancelBuild()
         }
     }
 
     @Test
     fun test_projectBuild_navigationDrawerProject_kotlin() {
         run {
-            step("Initialize the app") {
-                scenario(NavigateToMainScreenScenario())
-            }
-            clickCreateProject()
+            navigateToMainScreen()
+            clickCreateProjectHomeScreen()
             step("Select the navigation drawer project") {
                 selectTemplate(R.string.template_navigation_drawer)
             }
             selectKotlinLanguage()
-            step("Initialization the project and cancelling the build") {
-                scenario(InitializationProjectAndCancelingBuildScenario())
+            clickCreateProjectProjectSettings()
+            initializeProjectAndCancelBuild()
+        }
+    }
+
+    @Test
+    fun test_projectBuild_bottomNavigationProject_java() {
+        run {
+            navigateToMainScreen()
+            clickCreateProjectHomeScreen()
+            step("Select the navigation drawer project") {
+                selectTemplate(R.string.template_navigation_tabs)
             }
+            selectJavaLanguage()
+            clickCreateProjectProjectSettings()
+            initializeProjectAndCancelBuild()
+        }
+    }
+
+    @Test
+    fun test_projectBuild_bottomNavigationProject_kotlin() {
+        run {
+            navigateToMainScreen()
+            clickCreateProjectHomeScreen()
+            step("Select the navigation drawer project") {
+                selectTemplate(R.string.template_navigation_tabs)
+            }
+            selectKotlinLanguage()
+            clickCreateProjectProjectSettings()
+            initializeProjectAndCancelBuild()
+        }
+    }
+
+    private fun TestContext<Unit>.navigateToMainScreen() {
+        step("Initialize the app and navigate to the Main Screen") {
+            scenario(NavigateToMainScreenScenario())
+        }
+    }
+
+    private fun TestContext<Unit>.initializeProjectAndCancelBuild() {
+        step("Initialization the project and cancelling the build") {
+            scenario(InitializationProjectAndCancelingBuildScenario())
         }
     }
 }
