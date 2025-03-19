e016428b7bd70ece76a518bf9c6fe8384f70913e
alome007 <astrocoder007@gmail.com>
2025-03-19 07:59:58 +0100

    ADFA-519: WIP Fix build.yml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,7 +99,7 @@ tasks.register("createSymbolicLinkForLayoutEditor") {
     val destinationPath: java.nio.file.Path = Paths.get(rootDir.absolutePath, "layouteditor")
     val destinationFile = destinationPath.toFile()
 
-    if (os.contains("linux") || os.contains("nix") || os.contains("nux")) {
+    if (os.contains("linux") || os.contains("nix") || os.contains("nux") || os.contains("mac")) {
         // Check if the symbolic link already exists
         if (destinationFile.exists() && Files.isSymbolicLink(destinationPath)) {
             doLast {
