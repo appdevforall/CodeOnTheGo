53195cd5736a5c596052344de9b3560fcde17d5c
John Trujillo <john.trujillo@>
2025-03-21 09:15:27 -0500

    fix: Remove unused imports in CustomSnackbar.kt

--- app/src/main/java/com/itsaky/androidide/ui/CustomSnackbar.kt
+++ app/src/main/java/com/itsaky/androidide/ui/CustomSnackbar.kt
@@ -5,8 +5,6 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
-import android.widget.FrameLayout
-import android.widget.LinearLayout
 import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import com.itsaky.androidide.R
