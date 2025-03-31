ad09134d30c93b8591c4acbf64d29cf6cbba6557
James Turner <thursdaynext@gmail.com>
2025-03-31 16:01:05 -0500

    initial clone

--- app/src/main/java/com/itsaky/androidide/utils/TooltipUtils.kt
+++ app/src/main/java/com/itsaky/androidide/utils/TooltipUtils.kt
@@ -95,6 +95,8 @@ object TooltipUtils {
             else -> ""
         }
 
+        var pdfurl = ""
+
         //Dismiss the old PopupWindow
         fab.setOnClickListener {
             popupWindow.dismiss()
@@ -111,8 +113,35 @@ object TooltipUtils {
 
 
         val htmlString =
-            "<head><style type=\"text/css\"> html, body { width:100%; height: 100%; margin: 0px; padding: 0px; }" +
+            "<head><style type=\"text/css\"> html, body { width:100%; height: 100%; " +
+                    "margin: 0px; padding: 0px; }" +
                     "</style></head><body>$tooltip</body>"
+        val htmlPDFString =  """ 
+            <!DOCTYPE html> 
+            <html> 
+            <head> 
+                <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"> 
+                <style> 
+                    body, html { 
+                        margin: 0; 
+                        height: 100%; 
+                        overflow: hidden; 
+                    } 
+                    iframe { 
+                        position: absolute; 
+                        top: 0; 
+                        left: 0; 
+                        width: 100%; 
+                        height: 100%; 
+                        border: none; 
+                    } 
+                </style> 
+            </head> 
+            <body> 
+                <iframe src="$pdfurl" allow="autoplay"></iframe> 
+            </body> 
+            </html> 
+        """
 
         // Initialize the WebView
         val webView = popupView.findViewById<WebView>(R.id.webview)
