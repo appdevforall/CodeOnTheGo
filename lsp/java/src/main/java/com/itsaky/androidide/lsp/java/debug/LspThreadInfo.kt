c2df014b973c129e26d917b78ffb3d341a5f438a
Akash Yadav <akashyadav@appdevforall.org>
2025-05-01 15:40:43 +0530

    feat(lsp/debug): add base APIs for changing variable values
    Signed-off-by: Akash Yadav <akashyadav@appdevforall.org>

--- lsp/java/src/main/java/com/itsaky/androidide/lsp/java/debug/LspThreadInfo.kt
+++ lsp/java/src/main/java/com/itsaky/androidide/lsp/java/debug/LspThreadInfo.kt
@@ -1,5 +1,6 @@
 package com.itsaky.androidide.lsp.java.debug
 
+import com.itsaky.androidide.lsp.debug.model.LocalVariableValue
 import com.sun.jdi.LocalVariable
 import com.sun.jdi.StackFrame
 import com.itsaky.androidide.lsp.debug.model.LocalVariable as LspLocalVariable
@@ -9,29 +10,43 @@ import com.itsaky.androidide.lsp.debug.model.ThreadInfo as LspThreadInfo
 class JavaLocalVariable(
     val frame: StackFrame,
     val variable: LocalVariable,
-): LspLocalVariable {
+) : LspLocalVariable {
     override val name: String
         get() = variable.name()
 
     override val type: String
         get() = variable.typeName()
 
-    override fun getValue(): String =
-        frame.getValue(variable).toString()
+    override fun getValue(): LocalVariableValue =
+        object : LocalVariableValue {
+            override fun toString(): String =
+                frame.getValue(variable).toString()
+        }
+
+    override fun setValue(value: LocalVariableValue) {
+        TODO("Not yet implemented")
+    }
 }
 
 class JavaStackFrame(
     val frame: StackFrame,
-): LspStackFrame {
+) : LspStackFrame {
 
     override fun getVariables(): List<LspLocalVariable> =
         this.frame.visibleVariables().map { variable -> JavaLocalVariable(frame, variable) }
 
-    override fun getVariableValues(): Map<LspLocalVariable, String> =
+    override fun getVariableValues(): Map<LspLocalVariable, LocalVariableValue> =
         getVariables().associateWith { variable -> variable.getValue() }
 
-    override fun getValue(variable: LspLocalVariable): String =
+    override fun getValue(variable: LspLocalVariable): LocalVariableValue =
         (variable as JavaLocalVariable).getValue()
+
+    override fun setValue(
+        variable: LspLocalVariable,
+        value: LocalVariableValue
+    ) {
+        (variable as JavaLocalVariable).setValue(value)
+    }
 }
 
 internal class LspThreadInfo(
