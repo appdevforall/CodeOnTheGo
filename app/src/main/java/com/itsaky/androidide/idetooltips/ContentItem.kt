814376fae1302ecb6ada45c6aa8069ec8f3fd17a
Daniel-ADFA <danielalome@appdevforall.org>
2025-05-15 19:39:29 +0100

    Task/adfa 731 new pipeline documentation (#131)
    * Removed DS_Store from android-sdk.zip
    
    * ADFA-427 | feat Replace custom snackbar with BasicDialogController for app launch confirmation
    
    
    Co-authored-by: James Turner <thursdaynext@gmail.com>
    Co-authored-by: John Trujillo <johnandrestrujillo@gmail.com>
    Co-authored-by: Joel Menchavez <joelmenchavez@appdevforall.org>
    Co-authored-by: alome007 <astrocoder007@gmail.com>

---
+++ app/src/main/java/com/itsaky/androidide/idetooltips/ContentItem.kt
@@ -0,0 +1,38 @@
+package com.itsaky.androidide.idetooltips
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+
+// Define the data entities (same as before)
+@Entity(
+    tableName = "Content",
+    primaryKeys = ["path", "languageID"],
+    foreignKeys = [
+        ForeignKey(entity = Language::class, parentColumns = ["id"], childColumns = ["languageID"]),
+        ForeignKey(entity = ContentType::class, parentColumns = ["id"], childColumns = ["contentTypeID"])
+    ]
+)
+data class Content(
+    @ColumnInfo(name = "path") val path: String,
+    @ColumnInfo(name = "languageID") val languageID: Int,
+    @ColumnInfo(name = "content") val content: ByteArray,
+    @ColumnInfo(name = "contentTypeID") val contentTypeID: Int
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Content
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = languageID
+        result = 31 * result + contentTypeID
+        result = 31 * result + path.hashCode()
+        result = 31 * result + content.contentHashCode()
+        return result
+    }
+}
