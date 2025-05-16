a4540a77b04a30b984f7527a2099ae7063d79b1c
Akash Yadav <akashyadav@appdevforall.org>
2025-05-16 18:39:17 +0530

    feat(lsp/debug): define variable value types
    Signed-off-by: Akash Yadav <akashyadav@appdevforall.org>

--- lsp/api/src/main/java/com/itsaky/androidide/lsp/debug/model/ThreadInfo.kt
+++ lsp/api/src/main/java/com/itsaky/androidide/lsp/debug/model/ThreadInfo.kt
@@ -12,32 +12,6 @@ interface HasThreadInfo {
 }
 
 /**
- * Information about a local variable.
- */
-interface LocalVariable {
-
-    /**
-     * The name of the variable.
-     */
-    val name: String
-
-    /**
-     * The type of the variable.
-     */
-    val type: String
-
-    /**
-     * Get the value of the variable.
-     */
-    fun getValue(): String
-
-    /**
-     * Set the value of the variable.
-     */
-    fun setValue(value: String)
-}
-
-/**
  * A stack frame in the call stack.
  */
 interface StackFrame {
@@ -45,17 +19,7 @@ interface StackFrame {
     /**
      * Get the visible variables in this call frame.
      */
-    fun getVariables(): List<LocalVariable>
-
-    /**
-     * Get the values of the visible variables in this call frame.
-     */
-    fun getVariableValues(): Map<LocalVariable, String>
-
-    /**
-     * Get the value of the given variable.
-     */
-    fun getValue(variable: LocalVariable): String
+    fun getVariables(): List<Variable>
 
     /**
      * Set the value of the given variable.
@@ -63,7 +27,7 @@ interface StackFrame {
      * @param variable The variable to set the value of.
      * @param value The value to set the variable to.
      */
-    fun setValue(variable: LocalVariable, value: String)
+    fun setValue(variable: Variable, value: Value)
 }
 
 /**
