ccb176a5586ef2f5879b4bdc32de2ea0475fcb6c
Akash Yadav <akashyadav@appdevforall.org>
2025-05-27 17:16:04 +0530

    feat(debug): add tree view for variables
    Signed-off-by: Akash Yadav <akashyadav@appdevforall.org>

--- app/src/main/java/com/itsaky/androidide/fragments/debug/VariableListFragment.kt
+++ app/src/main/java/com/itsaky/androidide/fragments/debug/VariableListFragment.kt
@@ -4,20 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.setPadding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import com.itsaky.androidide.R
+import com.itsaky.androidide.common.databinding.LayoutSimpleIconTextBinding
+import com.itsaky.androidide.lsp.debug.model.Variable
 import com.itsaky.androidide.viewmodel.DebuggerViewModel
-import io.github.dingyi222666.view.treeview.Tree
+import io.github.dingyi222666.view.treeview.TreeNode
+import io.github.dingyi222666.view.treeview.TreeNodeEventListener
 import io.github.dingyi222666.view.treeview.TreeView
+import io.github.dingyi222666.view.treeview.TreeViewBinder
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * @author Akash Yadav
  */
 class VariableListFragment : Fragment() {
 
-    private lateinit var treeView: TreeView<VariablesTreeNode>
+    private lateinit var treeView: TreeView<Variable<*>>
 
     private val viewModel by viewModels<DebuggerViewModel>(ownerProducer = { requireActivity() })
 
@@ -37,26 +46,55 @@ class VariableListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.observeLatestSelectedFrame(
-            notifyOn = Dispatchers.Default
-        ) { _, _ ->
-            treeView.refresh()
+        treeView.apply {
+            supportHorizontalScroll = true
+            supportDragging = false
+            binder = VariableListBinder(viewLifecycleOwner.lifecycleScope)
+
+            bindCoroutineScope(viewLifecycleOwner.lifecycleScope)
+
+            nodeEventListener = object : TreeNodeEventListener<Variable<*>> {
+            }
         }
 
         viewModel.observeLatestVariablesTree(
             notifyOn = Dispatchers.Default
         ) { tree ->
-            if (::treeView.isInitialized) {
-                treeView.tree = tree
-                treeView.refresh()
-            }
+            treeView.tree = tree
+            treeView.refresh()
         }
+    }
+}
 
-        treeView.apply {
-            supportHorizontalScroll = true
-            supportDragging = false
+class VariableListBinder(
+    private val coroutineScope: CoroutineScope
+) : TreeViewBinder<Variable<*>>() {
 
-            bindCoroutineScope(viewLifecycleOwner.lifecycleScope)
+    override fun createView(parent: ViewGroup, viewType: Int): View {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = LayoutSimpleIconTextBinding.inflate(inflater, parent, false)
+        return binding.root
+    }
+
+    override fun getItemViewType(node: TreeNode<Variable<*>>): Int = 0
+
+    override fun bindView(
+        holder: TreeView.ViewHolder,
+        node: TreeNode<Variable<*>>,
+        listener: TreeNodeEventListener<Variable<*>>
+    ) {
+        val binding = LayoutSimpleIconTextBinding.bind(holder.itemView)
+        binding.root.apply {
+            setPaddingRelative(node.depth * binding.root.context.resources.getDimensionPixelSize(
+                R.dimen.content_padding_double), paddingTop, paddingEnd, paddingBottom)
+        }
+        coroutineScope.launch {
+            val label = node.data?.let {
+                "${it.name}: ${it.typeName}"
+            } ?: "<unknown>"
+            withContext(Dispatchers.Main) {
+                binding.text.text = label
+            }
         }
     }
 }
