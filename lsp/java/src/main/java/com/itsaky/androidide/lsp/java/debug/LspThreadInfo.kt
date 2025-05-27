a992cad0819ea99f9cfa0beb15733d972b7f27f0
Akash Yadav <akashyadav@appdevforall.org>
2025-05-27 18:57:27 +0530

    feat(lsp/debug/java): implement variable kind APIs
    Signed-off-by: Akash Yadav <akashyadav@appdevforall.org>

--- lsp/java/src/main/java/com/itsaky/androidide/lsp/java/debug/LspThreadInfo.kt
+++ lsp/java/src/main/java/com/itsaky/androidide/lsp/java/debug/LspThreadInfo.kt
@@ -1,79 +1,56 @@
 package com.itsaky.androidide.lsp.java.debug
 
-import com.itsaky.androidide.lsp.java.debug.utils.booleanValue
-import com.itsaky.androidide.lsp.java.debug.utils.byteValue
-import com.itsaky.androidide.lsp.java.debug.utils.charValue
-import com.itsaky.androidide.lsp.java.debug.utils.doubleValue
-import com.itsaky.androidide.lsp.java.debug.utils.floatValue
-import com.itsaky.androidide.lsp.java.debug.utils.intValue
-import com.itsaky.androidide.lsp.java.debug.utils.longValue
-import com.itsaky.androidide.lsp.java.debug.utils.shortValue
-import com.sun.jdi.LocalVariable
+import com.itsaky.androidide.lsp.debug.model.PrimitiveKind
+import com.itsaky.androidide.lsp.debug.model.PrimitiveValue
+import com.itsaky.androidide.lsp.debug.model.StringValue
+import com.itsaky.androidide.lsp.debug.model.Value
+import com.itsaky.androidide.lsp.debug.model.Variable
+import com.itsaky.androidide.lsp.debug.model.VariableKind
 import com.sun.jdi.StackFrame
-import com.itsaky.androidide.lsp.debug.model.LocalVariable as LspLocalVariable
 import com.itsaky.androidide.lsp.debug.model.StackFrame as LspStackFrame
 import com.itsaky.androidide.lsp.debug.model.ThreadInfo as LspThreadInfo
-
-class JavaLocalVariable(
-    val frame: StackFrame,
-    val variable: LocalVariable,
-) : LspLocalVariable {
-
-    companion object {
-        private val BOOLEAN_VALUES = setOf("true", "false")
-    }
-
-    override val name: String
-        get() = variable.name()
-
-    override val type: String
-        get() = variable.typeName()
-
-    override fun getValue(): String =
-        frame.getValue(variable).toString()
-
-    override fun setValue(value: String) {
-        val typeSignature = variable.signature()
-        val vm = frame.virtualMachine()
-
-        // TODO: Allow users to use references to existing variables instead of
-        //    just constant primitive values
-        val newValue = when (typeSignature[0]) {
-            'Z' -> vm.booleanValue(value)
-            'B' -> vm.byteValue(value)
-            'C' -> vm.charValue(value)
-            'S' -> vm.shortValue(value)
-            'I' -> vm.intValue(value)
-            'J' -> vm.longValue(value)
-            'F' -> vm.floatValue(value)
-            'D' -> vm.doubleValue(value)
-            '[' -> TODO("Add support for array values")
-            'L' -> TODO("Add support for object values")
-            else -> throw IllegalArgumentException("Unsupported variable type: $typeSignature")
-        }
-
-        frame.setValue(variable, newValue)
-    }
-}
+import com.itsaky.androidide.lsp.debug.model.Variable as LspVariable
 
 class JavaStackFrame(
     val frame: StackFrame,
 ) : LspStackFrame {
 
-    override fun getVariables(): List<LspLocalVariable> =
-        this.frame.visibleVariables().map { variable -> JavaLocalVariable(frame, variable) }
+    override fun getVariables(): List<LspVariable<*>> =
+        this.frame.visibleVariables().map { variable -> JavaLocalVariable.forVariable(frame, variable) }
 
-    override fun getVariableValues(): Map<LspLocalVariable, String> =
-        getVariables().associateWith { variable -> variable.getValue() }
+    override fun <Val : Value> setValue(variable: Variable<Val>, value: Val) {
+        variable as JavaLocalVariable
+        when (variable.kind) {
+            VariableKind.PRIMITIVE -> {
+                check(value is PrimitiveValue) {
+                    "Value $value is not a primitive value"
+                }
 
-    override fun getValue(variable: LspLocalVariable): String =
-        (variable as JavaLocalVariable).getValue()
+                variable as JavaPrimitiveVariable
+                when (variable.primitiveKind) {
+                    PrimitiveKind.BOOLEAN -> variable.setValue(value.asBoolean())
+                    PrimitiveKind.BYTE -> variable.setValue(value.asByte())
+                    PrimitiveKind.CHAR -> variable.setValue(value.asChar())
+                    PrimitiveKind.SHORT -> variable.setValue(value.asShort())
+                    PrimitiveKind.INT -> variable.setValue(value.asInt())
+                    PrimitiveKind.LONG -> variable.setValue(value.asLong())
+                    PrimitiveKind.FLOAT -> variable.setValue(value.asFloat())
+                    PrimitiveKind.DOUBLE -> variable.setValue(value.asDouble())
+                }
+            }
 
-    override fun setValue(
-        variable: LspLocalVariable,
-        value: String
-    ) {
-        (variable as JavaLocalVariable).setValue(value)
+            VariableKind.STRING -> {
+                check(value is StringValue) {
+                    "Value $value is not a string value"
+                }
+
+                variable as JavaStringVariable
+                variable.setValue(value.asString())
+            }
+
+            // TODO: Support other types of variable values
+            else -> throw IllegalStateException("Unsupported variable kind: ${variable.kind}")
+        }
     }
 }
 
