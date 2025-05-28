25bc21ed1d81458346252e17153314f7ce3eada1
Akash Yadav <akashyadav@appdevforall.org>
2025-05-28 20:36:40 +0530

    feat(lsp/debug): add eagerly-resolved variables and variable item views in debugger UI
    Signed-off-by: Akash Yadav <akashyadav@appdevforall.org>

--- lsp/java/src/main/java/com/itsaky/androidide/lsp/java/debug/LspThreadInfo.kt
+++ lsp/java/src/main/java/com/itsaky/androidide/lsp/java/debug/LspThreadInfo.kt
@@ -7,6 +7,8 @@ import com.itsaky.androidide.lsp.debug.model.Value
 import com.itsaky.androidide.lsp.debug.model.Variable
 import com.itsaky.androidide.lsp.debug.model.VariableKind
 import com.sun.jdi.StackFrame
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import com.itsaky.androidide.lsp.debug.model.StackFrame as LspStackFrame
 import com.itsaky.androidide.lsp.debug.model.ThreadInfo as LspThreadInfo
 import com.itsaky.androidide.lsp.debug.model.Variable as LspVariable
@@ -15,43 +17,46 @@ class JavaStackFrame(
     val frame: StackFrame,
 ) : LspStackFrame {
 
-    override fun getVariables(): List<LspVariable<*>> =
-        this.frame.visibleVariables().map { variable -> JavaLocalVariable.forVariable(frame, variable) }
-
-    override fun <Val : Value> setValue(variable: Variable<Val>, value: Val) {
-        variable as JavaLocalVariable
-        when (variable.kind) {
-            VariableKind.PRIMITIVE -> {
-                check(value is PrimitiveValue) {
-                    "Value $value is not a primitive value"
-                }
-
-                variable as JavaPrimitiveVariable
-                when (variable.primitiveKind) {
-                    PrimitiveKind.BOOLEAN -> variable.setValue(value.asBoolean())
-                    PrimitiveKind.BYTE -> variable.setValue(value.asByte())
-                    PrimitiveKind.CHAR -> variable.setValue(value.asChar())
-                    PrimitiveKind.SHORT -> variable.setValue(value.asShort())
-                    PrimitiveKind.INT -> variable.setValue(value.asInt())
-                    PrimitiveKind.LONG -> variable.setValue(value.asLong())
-                    PrimitiveKind.FLOAT -> variable.setValue(value.asFloat())
-                    PrimitiveKind.DOUBLE -> variable.setValue(value.asDouble())
-                }
-            }
-
-            VariableKind.STRING -> {
-                check(value is StringValue) {
-                    "Value $value is not a string value"
-                }
-
-                variable as JavaStringVariable
-                variable.setValue(value.asString())
-            }
-
-            // TODO: Support other types of variable values
-            else -> throw IllegalStateException("Unsupported variable kind: ${variable.kind}")
-        }
+    override suspend fun getVariables(): List<LspVariable<*>> {
+        val variables = withContext(Dispatchers.IO) { frame.visibleVariables() }
+        return variables.map { variable -> JavaLocalVariable.forVariable(frame, variable) }
     }
+
+    override suspend fun <Val : Value> setValue(variable: Variable<Val>, value: Val) =
+        withContext(Dispatchers.IO) {
+            variable as JavaLocalVariable
+            when (variable.kind) {
+                VariableKind.PRIMITIVE -> {
+                    check(value is PrimitiveValue) {
+                        "Value $value is not a primitive value"
+                    }
+
+                    variable as JavaPrimitiveVariable
+                    when (variable.primitiveKind) {
+                        PrimitiveKind.BOOLEAN -> variable.setValue(value.asBoolean())
+                        PrimitiveKind.BYTE -> variable.setValue(value.asByte())
+                        PrimitiveKind.CHAR -> variable.setValue(value.asChar())
+                        PrimitiveKind.SHORT -> variable.setValue(value.asShort())
+                        PrimitiveKind.INT -> variable.setValue(value.asInt())
+                        PrimitiveKind.LONG -> variable.setValue(value.asLong())
+                        PrimitiveKind.FLOAT -> variable.setValue(value.asFloat())
+                        PrimitiveKind.DOUBLE -> variable.setValue(value.asDouble())
+                    }
+                }
+
+                VariableKind.STRING -> {
+                    check(value is StringValue) {
+                        "Value $value is not a string value"
+                    }
+
+                    variable as JavaStringVariable
+                    variable.setValue(value.asString())
+                }
+
+                // TODO: Support other types of variable values
+                else -> throw IllegalStateException("Unsupported variable kind: ${variable.kind}")
+            }
+        }
 }
 
 internal class LspThreadInfo(
@@ -61,7 +66,7 @@ internal class LspThreadInfo(
     override fun getName(): String =
         thread.thread.name()
 
-    override fun getFrames(): List<LspStackFrame> =
+    override suspend fun getFrames(): List<LspStackFrame> =
         thread.frames()
             .map(::JavaStackFrame)
 }
