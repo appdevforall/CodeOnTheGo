79383b23d25dc63110423d5d06b0db6632bcb33f
Oluwadara Abijo <oluwadara.abijo@appdevforall.org>
2025-06-10 11:01:52 +0100

    ADFA-627: Create idetooltips module
    Create new module for tooltip implementation so the logic can be shared with both the app module and LayoutEditor

---
+++ idetooltips/build.gradle.kts
@@ -0,0 +1,23 @@
+import com.itsaky.androidide.build.config.BuildConfig
+
+plugins {
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.android.library)
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "${BuildConfig.packageName}.idetooltips"
+}
+
+dependencies {
+    kapt(libs.room.compiler)
+
+    implementation(libs.room.ktx)
+    implementation(libs.google.gson)
+    implementation(libs.google.guava)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.google.material)
+
+    implementation(projects.resources)
+}
