2396e35aa8cad5a9892b34e50326719ddada42e1
Akash Yadav <akashyadav@appdevforall.org>
2025-06-11 17:10:50 +0530

    fix: do not make IDEDebugClientImpl a singleton
    Signed-off-by: Akash Yadav <akashyadav@appdevforall.org>

--- app/src/main/java/com/itsaky/androidide/actions/debug/AbstractStepAction.kt
+++ app/src/main/java/com/itsaky/androidide/actions/debug/AbstractStepAction.kt
@@ -2,7 +2,6 @@ package com.itsaky.androidide.actions.debug
 
 import androidx.annotation.DrawableRes
 import com.itsaky.androidide.actions.ActionData
-import com.itsaky.androidide.lsp.IDEDebugClientImpl
 import com.itsaky.androidide.viewmodel.DebuggerConnectionState
 
 abstract class AbstractStepAction(
@@ -10,5 +9,5 @@ abstract class AbstractStepAction(
 ): AbstractDebuggerAction(iconRes) {
 
     override fun checkEnabled(data: ActionData): Boolean =
-        IDEDebugClientImpl.connectionState >= DebuggerConnectionState.AWAITING_BREAKPOINT
+        debugClient.connectionState >= DebuggerConnectionState.AWAITING_BREAKPOINT
 }
