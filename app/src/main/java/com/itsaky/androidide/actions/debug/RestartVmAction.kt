ac831a7a1dff4a4720ba1648ccc177cce74abd47
Akash Yadav <akashyadav@appdevforall.org>
2025-06-11 19:19:07 +0530

    fix: enhance debugger start process
    Signed-off-by: Akash Yadav <akashyadav@appdevforall.org>

--- app/src/main/java/com/itsaky/androidide/actions/debug/RestartVmAction.kt
+++ app/src/main/java/com/itsaky/androidide/actions/debug/RestartVmAction.kt
@@ -3,6 +3,8 @@ package com.itsaky.androidide.actions.debug
 import android.content.Context
 import com.itsaky.androidide.R
 import com.itsaky.androidide.actions.ActionData
+import kotlinx.coroutines.delay
+import kotlin.time.Duration.Companion.seconds
 
 /**
  * @author Akash Yadav
@@ -14,7 +16,13 @@ class RestartVmAction(
     override var label = context.getString(R.string.debugger_restart)
     override val order = 5
 
+    companion object {
+        val RESTART_DELAY = 1.seconds
+    }
+
     override suspend fun execAction(data: ActionData) {
-        // TODO: Restart VM
+        debugClient.killVm()
+        delay(RESTART_DELAY)
+
     }
 }
