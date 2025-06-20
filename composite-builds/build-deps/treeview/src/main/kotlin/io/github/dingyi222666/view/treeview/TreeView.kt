5c7f210645778bcb6541701cef5de560d80c79b1
John Trujillo <john.trujillo@>
2025-06-20 09:41:51 -0500

    feat: adding update variable tree and refresh treeView

--- composite-builds/build-deps/treeview/src/main/kotlin/io/github/dingyi222666/view/treeview/TreeView.kt
+++ composite-builds/build-deps/treeview/src/main/kotlin/io/github/dingyi222666/view/treeview/TreeView.kt
@@ -151,7 +151,7 @@ class TreeView<T : Any> @JvmOverloads constructor(
     private lateinit var _adapter: Adapter
     private val _itemTouchHelperCallback: ItemTouchHelperCallback
 
-    private var coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.Main)
+    var coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.Main)
 
     private var isHorizontalScrolling = false
     private var initialTouchX = 0f
