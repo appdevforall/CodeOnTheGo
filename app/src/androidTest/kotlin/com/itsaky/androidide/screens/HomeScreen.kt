bab2715b7dce2ececdd7ce04f20f6f2ceb675eff
Dara Abijo <oluwadara.abijo@appdevforall.org>
2025-06-27 14:08:40 +0100

    fix(ADFA-1093): Resolve failing test (#224)
    Co-authored-by: Daniel-ADFA <danielalome@appdevforall.org>

--- app/src/androidTest/kotlin/com/itsaky/androidide/screens/HomeScreen.kt
+++ app/src/androidTest/kotlin/com/itsaky/androidide/screens/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.itsaky.androidide.screens
 
 import android.view.View
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.itsaky.androidide.R
 import com.kaspersky.kaspresso.screens.KScreen
 import com.kaspersky.kaspresso.testcases.core.testcontext.TestContext
@@ -9,6 +10,7 @@ import io.github.kakaocup.kakao.recycler.KRecyclerView
 import io.github.kakaocup.kakao.text.KButton
 import io.github.kakaocup.kakao.text.KTextView
 import org.hamcrest.Matcher
+import org.hamcrest.Matchers
 
 object HomeScreen : KScreen<HomeScreen>() {
 
@@ -30,13 +32,21 @@ object HomeScreen : KScreen<HomeScreen>() {
     }
 
     fun TestContext<Unit>.clickCreateProjectHomeScreen() {
+
+        val createProjectLabel = "Create Project"
+        val getStartedLabel = "Get started"
+
         step("Click create project") {
             flakySafely(60000) {
                 HomeScreen {
                     try {
                         title {
                             isVisible()
-                            containsText("Get started")
+                            withText(
+                                Matchers.equalToIgnoringCase(
+                                    getStartedLabel
+                                )
+                            )
                         }
                     } catch (e: Exception) {
                         println("Get Started title not visible: ${e.message}")
@@ -44,7 +54,13 @@ object HomeScreen : KScreen<HomeScreen>() {
 
                     rvActions {
                         scrollTo(0)
-                        childWith<ActionItem> { withText("Create Project") } perform {
+                        childWith<ActionItem> {
+                            withText(
+                                Matchers.equalToIgnoringCase(
+                                    createProjectLabel
+                                )
+                            )
+                        } perform {
                             isDisplayed()
                             click()
                         }
