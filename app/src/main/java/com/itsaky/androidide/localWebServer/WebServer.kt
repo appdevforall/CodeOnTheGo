3b6201119a585c814a1520eef90d1b770b6b6d5c
Daniel-ADFA <danielalome@appdevforall.org>
2025-07-08 20:01:43 +0100

    ADFA-741: Refactor: Remove hardcoded database path and use dynamic path resolution (#237)

--- app/src/main/java/com/itsaky/androidide/localWebServer/WebServer.kt
+++ app/src/main/java/com/itsaky/androidide/localWebServer/WebServer.kt
@@ -14,7 +14,7 @@ import android.util.Log
 
 data class ServerConfig(
     val port: Int = 6174,
-    val databasePath: String = "/data/data/com.itsaky.androidide/databases/documentation.db"
+    val databasePath: String
 )
 
 class WebServer(private val config: ServerConfig) {
