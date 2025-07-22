12e9856b526a425c0f3fa21bc2440fa74f06cae7
Akash Yadav <akashyadav@appdevforall.org>
2025-07-22 22:04:07 +0530

    fix: disable coreLibraryDesugaring in :logsender and libjdwp-remote
    These projects are built and included as AAR files in the APK, which then projects built in CoGo depend on. Enabling coreLibraryDesugaring in these projects requires projects built in CoGo to enable coreLibraryDesugaring as well.
    
    Signed-off-by: Akash Yadav <akashyadav@appdevforall.org>

--- logsender/build.gradle.kts
+++ logsender/build.gradle.kts
@@ -36,6 +36,7 @@ android {
   }
 
   compileOptions {
+    isCoreLibraryDesugaringEnabled = false
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
   }
