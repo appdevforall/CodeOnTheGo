7ec537ee735100533a3b06087726cca451290a83
Joel Menchavez <joelmenchavez@appdevforall.org>
2025-08-06 16:57:30 +0800

    move extraction to doInstall

--- app/src/main/java/com/itsaky/androidide/assets/SplitAssetsInstaller.kt
+++ app/src/main/java/com/itsaky/androidide/assets/SplitAssetsInstaller.kt
@@ -22,99 +22,18 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.util.zip.ZipFile
 import java.util.zip.ZipInputStream
-import kotlin.io.path.exists
 import kotlin.system.measureTimeMillis
 
 data object SplitAssetsInstaller : BaseAssetsInstaller() {
 
     private val logger = LoggerFactory.getLogger(SplitAssetsInstaller::class.java)
+    private lateinit var zipFile: ZipFile
 
     override suspend fun preInstall(
         context: Context,
         stagingDir: Path,
     ): Unit = withContext(Dispatchers.IO) {
-        val zipFile = ZipFile(Environment.SPLIT_ASSETS_ZIP)
-
-        supervisorScope {
-            val jobs = zipFile.entries().toList().map { entry ->
-                launch {
-                    if (entry.isDirectory) return@launch
-
-                    val time = measureTimeMillis {
-                        zipFile.getInputStream(entry).use { zipInput ->
-                            when (entry.name) {
-                                GRADLE_DISTRIBUTION_ARCHIVE_NAME,
-                                ANDROID_SDK_ZIP,
-                                LOCAL_MAVEN_REPO_ARCHIVE_ZIP_NAME,
-                                GRADLE_API_NAME_JAR_ZIP -> {
-                                    val destDir = destinationDirForArchiveEntry(entry.name).toPath()
-                                    logger.debug("Extracting '{}' to dir: {}", entry.name, destDir)
-                                    ZipInputStream(zipInput).useEntriesEach { innerZipInput, innerEntry ->
-                                        val innerFile = destDir.resolve(innerEntry.name)
-                                        if (innerEntry.isDirectory) {
-                                            Files.createDirectories(innerFile)
-                                        } else {
-                                            if (innerFile.parent != null) {
-                                                Files.createDirectories(innerFile.parent)
-                                            }
-                                            Files.newOutputStream(innerFile).use { out ->
-                                                innerZipInput.copyTo(out)
-                                            }
-                                        }
-                                    }
-                                    logger.debug("Completed extracting '{}' to dir: {}", entry.name, destDir)
-                                }
-
-                                AssetsInstallationHelper.BOOTSTRAP_ENTRY_NAME -> {
-                                    logger.debug("Extracting 'bootstrap.zip' to dir: {}", stagingDir)
-
-                                    // We need a SeekableByteChannel for the TerminalInstaller, but the ZipInputStream is not seekable.
-                                    // The only way is to write it to a temporary file first.
-                                    val tempBootstrap = Files.createTempFile(stagingDir, "bootstrap", ".zip")
-                                    try {
-                                        Files.newOutputStream(tempBootstrap).use { out ->
-                                            zipInput.copyTo(out)
-                                        }
-                                        val channel = Files.newByteChannel(tempBootstrap)
-                                        val result = TerminalInstaller.installIfNeeded(context, channel)
-                                        if (result !is TerminalInstaller.InstallResult.Success) {
-                                            // Log the error and continue with other assets.
-                                            logger.error("Failed to install terminal: $result")
-                                        }
-                                    } finally {
-                                        Files.deleteIfExists(tempBootstrap)
-                                    }
-                                    logger.debug("Completed extracting 'bootstrap.zip' to dir: {}", stagingDir)
-                                }
-
-                                DOCUMENTATION_DB -> {
-                                    logger.debug("Extracting '{}' to {}", DOCUMENTATION_DB, Environment.DOC_DB)
-
-                                    // prefer already extracted documentation.db -- only in debug builds
-                                    val splitDb = Environment.DOWNLOAD_DIR.resolve(DOCUMENTATION_DB)
-                                    if (splitDb.exists()) {
-                                        splitDb.inputStream()
-                                    } else {
-                                        zipInput
-                                    }.use { stream ->
-                                        Environment.DOC_DB.outputStream().use { output ->
-                                            stream.copyTo(output)
-                                        }
-                                    }
-                                    logger.debug("Completed extracting '{}' to {}", DOCUMENTATION_DB, Environment.DOC_DB)
-                                }
-
-                                else -> logger.warn("Unknown entry in assets zip: {}", entry.name)
-                            }
-                        }
-                    }
-                    logger.info("Extraction of '{}' completed in {}ms", entry.name, time)
-                }
-            }
-            jobs.joinAll()
-        }
-
-        zipFile.close()
+        zipFile = ZipFile(Environment.SPLIT_ASSETS_ZIP)
     }
 
     @WorkerThread
@@ -124,7 +43,86 @@ data object SplitAssetsInstaller : BaseAssetsInstaller() {
         cpuArch: CpuArch,
         entryName: String,
     ): Unit = withContext(Dispatchers.IO) {
-        // All work is done in preInstall.
+        val entry = zipFile.getEntry(entryName)
+        val time = measureTimeMillis {
+            zipFile.getInputStream(entry).use { zipInput ->
+                when (entry.name) {
+                    GRADLE_DISTRIBUTION_ARCHIVE_NAME,
+                    ANDROID_SDK_ZIP,
+                    LOCAL_MAVEN_REPO_ARCHIVE_ZIP_NAME,
+                    GRADLE_API_NAME_JAR_ZIP -> {
+                        val destDir = destinationDirForArchiveEntry(entry.name).toPath()
+                        logger.debug("Extracting '{}' to dir: {}", entry.name, destDir)
+                        ZipInputStream(zipInput).useEntriesEach { innerZipInput, innerEntry ->
+                            val innerFile = destDir.resolve(innerEntry.name)
+                            if (innerEntry.isDirectory) {
+                                Files.createDirectories(innerFile)
+                            } else {
+                                if (innerFile.parent != null) {
+                                    Files.createDirectories(innerFile.parent)
+                                }
+                                Files.newOutputStream(innerFile).use { out ->
+                                    innerZipInput.copyTo(out)
+                                }
+                            }
+                        }
+                        logger.debug("Completed extracting '{}' to dir: {}", entry.name, destDir)
+                    }
+
+                    AssetsInstallationHelper.BOOTSTRAP_ENTRY_NAME -> {
+                        logger.debug("Extracting 'bootstrap.zip' to dir: {}", stagingDir)
+
+                        // We need a SeekableByteChannel for the TerminalInstaller, but the ZipInputStream is not seekable.
+                        // The only way is to write it to a temporary file first.
+                        val tempBootstrap = Files.createTempFile(stagingDir, "bootstrap", ".zip")
+                        try {
+                            Files.newOutputStream(tempBootstrap).use { out ->
+                                zipInput.copyTo(out)
+                            }
+                            val channel = Files.newByteChannel(tempBootstrap)
+                            val result = TerminalInstaller.installIfNeeded(context, channel)
+                            if (result !is TerminalInstaller.InstallResult.Success) {
+                                // Log the error and continue with other assets.
+                                logger.error("Failed to install terminal: $result")
+                            }
+                        } finally {
+                            Files.deleteIfExists(tempBootstrap)
+                        }
+                        logger.debug("Completed extracting 'bootstrap.zip' to dir: {}", stagingDir)
+                    }
+
+                    DOCUMENTATION_DB -> {
+                        logger.debug("Extracting '{}' to {}", DOCUMENTATION_DB, Environment.DOC_DB)
+
+                        // prefer already extracted documentation.db -- only in debug builds
+                        val splitDb = Environment.DOWNLOAD_DIR.resolve(DOCUMENTATION_DB)
+                        if (splitDb.exists()) {
+                            splitDb.inputStream()
+                        } else {
+                            zipInput
+                        }.use { stream ->
+                            Environment.DOC_DB.outputStream().use { output ->
+                                stream.copyTo(output)
+                            }
+                        }
+                        logger.debug("Completed extracting '{}' to {}", DOCUMENTATION_DB, Environment.DOC_DB)
+                    }
+
+                    else -> logger.warn("Unknown entry in assets zip: {}", entry.name)
+                }
+            }
+        }
+        logger.info("Extraction of '{}' completed in {}ms", entry.name, time)
+    }
+
+    override suspend fun postInstall(
+        context: Context,
+        stagingDir: Path
+    ) {
+        withContext(Dispatchers.IO) {
+            zipFile.close()
+        }
+        super.postInstall(context, stagingDir)
     }
 
     private fun destinationDirForArchiveEntry(entryName: String): File = when (entryName) {
