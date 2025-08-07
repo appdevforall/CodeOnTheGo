6f229b4581974f4a664354aee0af05a8d0da9fd5
Oluwadara Abijo <oluwadara.abijo@appdevforall.org>
2025-08-08 00:26:16 +0100

    refactor(ADFA-1191): Move submodule files to new package

---
+++ layouteditor-module/src/main/java/org/appdevforall/layouteditor/LayoutEditor.kt
@@ -0,0 +1,38 @@
+package com.itsvks.layouteditor
+
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+import android.os.Build
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.color.DynamicColors
+import com.itsvks.layouteditor.managers.PreferencesManager
+
+class LayoutEditor : Application() {
+
+  private lateinit var prefManager: PreferencesManager
+  override fun onCreate() {
+    super.onCreate()
+    instance = this
+    prefManager = PreferencesManager(this.context)
+    AppCompatDelegate.setDefaultNightMode(prefManager.currentTheme)
+    if (prefManager.isApplyDynamicColors && DynamicColors.isDynamicColorAvailable()) {
+      DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+  }
+
+  val context: Context
+    get() = instance!!.applicationContext
+  val isAtLeastTiramisu: Boolean
+    get() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+
+  fun updateTheme(nightMode: Int, activity: Activity) {
+    AppCompatDelegate.setDefaultNightMode(nightMode)
+    activity.recreate()
+  }
+
+  companion object {
+    var instance: LayoutEditor? = null
+      private set
+  }
+}
