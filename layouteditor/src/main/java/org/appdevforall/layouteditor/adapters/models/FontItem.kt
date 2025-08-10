366503a012a62357c0a93636290732c90c354577
Oluwadara Abijo <oluwadara.abijo@appdevforall.org>
2025-08-10 23:47:04 +0100

    refactor(ADFA-1191): Update package names

--- layouteditor/src/main/java/org/appdevforall/layouteditor/adapters/models/FontItem.kt
+++
@@ -1,3 +0,0 @@
-package org.appdevforall.layouteditor.adapters.models
-
-data class FontItem(@JvmField var name: String, @JvmField var path: String)
--- layouteditor/src/main/java/org/appdevforall/layouteditor/editor/DeviceSize.kt
+++
@@ -1,7 +0,0 @@
-package org.appdevforall.layouteditor.editor
-
-enum class DeviceSize {
-  SMALL,
-  MEDIUM,
-  LARGE
-}
