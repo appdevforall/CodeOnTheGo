a07bda23237a694faf6120fd96507f41c5932010
alome007 <astrocoder007@gmail.com>
2025-08-15 15:50:46 +0100

    feat: refactor feature flag implementation into a companion object

--- common/src/main/java/com/itsaky/androidide/utils/FeatureFlags.kt
+++ common/src/main/java/com/itsaky/androidide/utils/FeatureFlags.kt
@@ -4,10 +4,14 @@ package com.itsaky.androidide.utils
 import android.os.Environment
 import java.io.File
 
-private const val EXPERIMENTS_FILE_NAME = "CodeOnTheGo.exp"
-
-fun isExperimentsEnabled(): Boolean {
-    val downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-    val experimentsFile = File(downloadsDir, EXPERIMENTS_FILE_NAME)
-    return experimentsFile.exists()
+class FeatureFlags {
+    companion object {
+        private const val EXPERIMENTS_FILE_NAME = "CodeOnTheGo.exp"
+        
+        fun isExperimentsEnabled(): Boolean {
+            val downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+            val experimentsFile = File(downloadsDir, EXPERIMENTS_FILE_NAME)
+            return experimentsFile.exists()
+        }
+    }
 }
