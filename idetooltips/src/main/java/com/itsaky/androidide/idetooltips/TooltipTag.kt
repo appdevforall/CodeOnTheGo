c131eaa1ad0c5d2dee1a7cdc54ac0740d2bea606
John Trujillo <johnandrestrujillo@gmail.com>
2025-08-20 08:52:28 -0500

    Refactor: Use TooltipTag for sidebar actions
    This commit refactors the sidebar actions to use the newly introduced `TooltipTag` object for managing tooltip tags.
    
    The following sidebar actions were updated:
    - TerminalSidebarAction
    - HelpSideBarAction
    - FileTreeSidebarAction
    - PreferencesSidebarAction
    - CloseProjectSidebarAction
    - BuildVariantsSidebarAction
    
    A new file `idetooltips/src/main/java/com/itsaky/androidide/idetooltips/TooltipTag.kt` was created to define the tooltip tags.

---
+++ idetooltips/src/main/java/com/itsaky/androidide/idetooltips/TooltipTag.kt
@@ -0,0 +1,10 @@
+package com.itsaky.androidide.idetooltips
+
+object TooltipTag {
+    const val BUILD_VARIANT_SIDEBAR = "project.sidebar.buildvariants"
+    const val CLOSE_PROJECT_SIDEBAR = "project.sidebar.closeproject"
+    const val FILE_TREE_SIDEBAR = "project.sidebar.filetree"
+    const val HELP_SIDEBAR = "project.help"
+    const val PREFERENCES_SIDEBAR = "main.prefs"
+    const val TERMINAL_SIDEBAR = "project.sidebar.terminal"
+}
