76c603439785bc331e0b22292b533ea4152564b0
Dara Abijo <oluwadara.abijo@appdevforall.org>
2025-08-25 12:31:35 +0100

    ADFA-1179: Display editor toolbar menu items in a scroll view (#247)
    * WIP(ADFA-307): create custom toolbar
    
    * fix (ADFA-307): remove duplicate menu items
    
    * feat(ADFA-307): Add arrows to the start and end of the scrollview
    
    * feat(ADFA-307): Replace extended toolbar with custom toolbar
    
    * feat(ADFA-1179): Remove app name from editor toolbar
    
    Remove "Code on the Go" from editor toolbar and display just the current project's name
    
    * feat(ADFA-1179): Replace double arrows with single arrows
    
    * feat(ADFA-1179): Handle find action
    
    * Show a dialog when "Find" menu is clicked
    * Implement "FindInFile" and "FindInProject" actions
    
    * fix(ADFA-1179): UI fixes in toolbar menu
    
    * refactor(ADFA-1179): Remove hardcoded text
    
    * refactor(ADFA-1179): Remove unnecessary context addition
    
    * refactor(ADFA-1179): Rename variable name
    
    Change "shouldMarkInvisible" to "shouldHideFindInFileAction"
    
    * refactor(ADFA-1179): Remove redundant variable declaration
    
    * refactor(ADFA-1179): Remove usages of ExtendedMenuToolbar
    
    * fix(ADFA-1179): Set image color tint based on device theme
    
    * refactor(ADFA-1179): Reorder editor toolbar menu items
    
    * feat(ADFA-1179): Update menu items when app state changes
    
    * fix(ADFA-1179): Always show chevrons
    
    * fix(ADFA-1179): Remove end margin from last action item
    
    * fix(ADFA-1179): Use cancel build icon while build is in progress only for quick run action
    
    Both QuickRunAction and DebugAction implement AbstractCancellableRunAction which sets the action item icon to "stop_daemons" icon. This is not needed for DebugAction.
    
    * fix(ADFA-1179): Disable debug action when build is in progress
    
    * feat(ADFA-1179): Remove git icons from toolbar
    
    * fix(ADFA-1179): Resolve issue with project name's text color in dark mode
    
    * refactor(ADFA-1179): Remove manual action execution
    
    * refactor(ADFA-1179): Replace findViewById with view binding
    
    * refactor(ADFA-1179): Use current theme attributes
    
    * fix(ADFA-1179): Prevent project title from overlapping toolbar actions
    
    * fix(ADFA-1179): Disable find action if build is in progress
    
    * fix(ADFA-1179): Execute actions only when item is enabled
    
    * fix(ADFA-1179): Fix issue with quick run action
    
    * refactor(ADFA-1179): Rename find action item location
    
    * refactor(ADFA-1179): Use theme-defined colors
    
    * fix: extract action IDs to constants
    
    Signed-off-by: Akash Yadav <akashyadav@appdevforall.org>
    
    ---------
    
    Signed-off-by: Akash Yadav <akashyadav@appdevforall.org>
    Co-authored-by: Akash Yadav <akashyadav@appdevforall.org>

--- app/src/main/java/com/itsaky/androidide/actions/build/RunTasksAction.kt
+++ app/src/main/java/com/itsaky/androidide/actions/build/RunTasksAction.kt
@@ -26,8 +26,12 @@ import com.itsaky.androidide.resources.R
 
 /** @author Akash Yadav */
 class RunTasksAction(context: Context, override val order: Int) : BaseBuildAction() {
-  override val id: String = "ide.editor.build.runTasks"
+  override val id: String = ID
   private var dialog: RunTasksDialogFragment? = null
+
+  companion object {
+    const val ID = "ide.editor.build.runTasks"
+  }
 
   init {
     label = context.getString(R.string.title_run_tasks)
