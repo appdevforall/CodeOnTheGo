c9f3037e2d06c4f9e17fc6979416f54a83ac170e
John Trujillo <johnandrestrujillo@gmail.com>
2025-09-01 08:35:05 -0500

    Refactor: Convert SymbolInputAdapter to Kotlin
    This commit converts the `SymbolInputAdapter.java` file to Kotlin.
    The functionality of the class remains the same.

--- app/src/main/java/com/itsaky/androidide/adapters/SymbolInputAdapter.kt
+++ app/src/main/java/com/itsaky/androidide/adapters/SymbolInputAdapter.kt
@@ -15,159 +15,154 @@
  * along with AndroidIDE.  If not, see <https://www.gnu.org/licenses/>.
  *
  */
-package com.itsaky.androidide.adapters;
+package com.itsaky.androidide.adapters
 
-import static com.itsaky.androidide.utils.ContextUtilsKt.resolveAttr;
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.widget.TooltipCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.itsaky.androidide.R
+import com.itsaky.androidide.databinding.LayoutSymbolItemBinding
+import com.itsaky.androidide.editor.ui.IDEEditor
+import com.itsaky.androidide.models.Symbol
+import com.itsaky.androidide.utils.resolveAttr
+import io.github.rosemoe.sora.widget.SelectionMovement
+import java.util.Objects
 
-import android.annotation.SuppressLint;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
-import android.widget.Toast;
+class SymbolInputAdapter @JvmOverloads constructor(
+    private var editor: IDEEditor,
+    symbols: Collection<Symbol>? = null
+) : RecyclerView.Adapter<SymbolInputAdapter.VH?>() {
+    private val symbols: MutableList<Symbol> = ArrayList()
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.widget.TooltipCompat;
-import androidx.recyclerview.widget.RecyclerView;
-import com.itsaky.androidide.R;
-import com.itsaky.androidide.databinding.LayoutSymbolItemBinding;
-import com.itsaky.androidide.editor.ui.IDEEditor;
-import com.itsaky.androidide.models.Symbol;
-import io.github.rosemoe.sora.widget.SelectionMovement;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
-public class SymbolInputAdapter extends RecyclerView.Adapter<SymbolInputAdapter.VH> {
-
-  private static final List<Character> pairs;
-
-  static {
-    pairs = new ArrayList<>();
-    pairs.add('}');
-    pairs.add(')');
-    pairs.add(']');
-    pairs.add('"');
-    pairs.add('\'');
-    pairs.add('>');
-  }
-
-  private IDEEditor editor;
-  private final List<Symbol> symbols;
-
-  public SymbolInputAdapter(IDEEditor editor) {
-    this(editor, null);
-  }
-
-  public SymbolInputAdapter(IDEEditor editor, List<Symbol> symbols) {
-    this.editor = editor;
-    this.symbols = new ArrayList<>();
-    this.updateItems(symbols);
-  }
-
-  private void updateItems(List<Symbol> symbols) {
-    if (symbols == null) {
-      return;
+    init {
+        this.updateItems(symbols)
     }
 
-    this.symbols.clear();
-    this.symbols.addAll(symbols);
-    this.symbols.removeIf(Objects::isNull);
-  }
+    private fun updateItems(symbols: Collection<Symbol>?) {
+        if (symbols == null) {
+            return
+        }
 
-  @SuppressLint("NotifyDataSetChanged")
-  public void refresh(IDEEditor editor, List<Symbol> newSymbols) {
-    this.editor = Objects.requireNonNull(editor);
-
-    if (this.symbols.equals(newSymbols)) {
-      // no need to update symbols
-      return;
+        this.symbols.clear()
+        this.symbols.addAll(symbols)
+        this.symbols.removeIf { obj: Symbol? -> Objects.isNull(obj) }
     }
 
-    updateItems(newSymbols);
-    notifyDataSetChanged();
-  }
+    @SuppressLint("NotifyDataSetChanged")
+    fun refresh(editor: IDEEditor?, newSymbols: Collection<Symbol>?) {
+        this.editor = Objects.requireNonNull<IDEEditor>(editor)
 
-  @NonNull
-  @Override
-  public VH onCreateViewHolder(@NonNull ViewGroup parent, int itemType) {
-    return new VH(
-        LayoutSymbolItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
-  }
+        if (this.symbols == newSymbols) {
+            // no need to update symbols
+            return
+        }
 
-  @Override
-  public void onBindViewHolder(@NonNull VH holder, int position) {
-    final Symbol symbol = symbols.get(position);
-    holder.binding.symbol.setText(symbol.getLabel());
-    holder.binding.symbol.setTextColor(
-            resolveAttr(holder.binding.symbol.getContext(), R.attr.colorOnSurface));
-
-    String description = symbol.getDescription();
-
-    if (!description.isEmpty()) {
-      TooltipCompat.setTooltipText(holder.binding.symbol, description);
+        updateItems(newSymbols)
+        notifyDataSetChanged()
     }
 
-    holder.binding.symbol.setOnClickListener(view ->
-            insertSymbol(symbol.getCommit(), symbol.getOffset())
-    );
-
-    holder.binding.symbol.setOnLongClickListener(view -> {
-      if (!description.isEmpty()) {
-        Toast.makeText(view.getContext(), description, Toast.LENGTH_SHORT).show();
-      }
-      return true;
-    });
-  }
-
-  @Override
-  public int getItemCount() {
-    return symbols.size();
-  }
-
-  void insertSymbol(String text, int selectionOffset) {
-    if (selectionOffset < 0 || selectionOffset > text.length()) {
-      return;
+    override fun onCreateViewHolder(parent: ViewGroup, itemType: Int): VH {
+        return VH(
+            LayoutSymbolItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
     }
 
-    final var controller = editor.getSnippetController();
-    if ("\t".equals(text) && controller.isInSnippet()) {
-      controller.shiftToNextTabStop();
-      return;
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        val symbol = symbols.get(position)
+        holder.binding.symbol.text = symbol.label
+        holder.binding.symbol.setTextColor(
+            holder.binding.symbol.context.resolveAttr(R.attr.colorOnSurface)
+        )
+
+        val description = symbol.description
+
+        if (!description.isEmpty()) {
+            TooltipCompat.setTooltipText(holder.binding.symbol, description)
+        }
+
+        holder.binding.symbol.setOnClickListener(View.OnClickListener { view: View? ->
+            insertSymbol(
+                symbol.commit,
+                symbol.offset
+            )
+        }
+        )
+
+        holder.binding.symbol.setOnLongClickListener(View.OnLongClickListener { view: View? ->
+            if (!description.isEmpty()) {
+                Toast.makeText(view!!.context, description, Toast.LENGTH_SHORT).show()
+            }
+            true
+        })
     }
 
-    if ("\t".equals(text)) {
-      editor.indentOrCommitTab();
-      return;
+    override fun getItemCount(): Int {
+        return symbols.size
     }
 
-    var cur = editor.getText().getCursor();
-    if (cur.isSelected()) {
-      editor
-          .getText()
-          .delete(cur.getLeftLine(), cur.getLeftColumn(), cur.getRightLine(), cur.getRightColumn());
-      editor.notifyIMEExternalCursorChange();
+    fun insertSymbol(text: String, selectionOffset: Int) {
+        if (selectionOffset < 0 || selectionOffset > text.length) {
+            return
+        }
+
+        val controller = editor.snippetController
+        if ("\t" == text && controller.isInSnippet()) {
+            controller.shiftToNextTabStop()
+            return
+        }
+
+        if ("\t" == text) {
+            editor.indentOrCommitTab()
+            return
+        }
+
+        val cur = editor.text.getCursor()
+        if (cur.isSelected) {
+            editor
+                .text
+                .delete(
+                    cur.leftLine,
+                    cur.leftColumn,
+                    cur.rightLine,
+                    cur.rightColumn
+                )
+            editor.notifyIMEExternalCursorChange()
+        }
+
+        if (cur.leftColumn < editor.text
+                .getColumnCount(cur.leftLine) && text.length == 1 && text.get(0) == editor.text
+                .charAt(cur.leftLine, cur.leftColumn) && pairs.contains(text.get(0))
+        ) {
+            editor.moveSelection(SelectionMovement.RIGHT)
+        } else {
+            editor.commitText(text)
+            if (selectionOffset != text.length) {
+                editor.setSelection(
+                    cur.rightLine, cur.rightColumn - (text.length - selectionOffset)
+                )
+            }
+        }
     }
 
-    if (cur.getLeftColumn() < editor.getText().getColumnCount(cur.getLeftLine())
-        && text.length() == 1
-        && text.charAt(0) == editor.getText().charAt(cur.getLeftLine(), cur.getLeftColumn())
-        && pairs.contains(text.charAt(0))) {
-      editor.moveSelection(SelectionMovement.RIGHT);
-    } else {
-      editor.commitText(text);
-      if (selectionOffset != text.length()) {
-        editor.setSelection(
-            cur.getRightLine(), cur.getRightColumn() - (text.length() - selectionOffset));
-      }
+    class VH(var binding: LayoutSymbolItemBinding) : RecyclerView.ViewHolder(
+        binding.getRoot()
+    )
+
+    companion object {
+        private val pairs: MutableList<Char?>
+
+        init {
+            pairs = ArrayList<Char?>()
+            pairs.add('}')
+            pairs.add(')')
+            pairs.add(']')
+            pairs.add('"')
+            pairs.add('\'')
+            pairs.add('>')
+        }
     }
-  }
-
-  public static class VH extends RecyclerView.ViewHolder {
-
-    LayoutSymbolItemBinding binding;
-
-    public VH(LayoutSymbolItemBinding binding) {
-      super(binding.getRoot());
-      this.binding = binding;
-    }
-  }
 }
