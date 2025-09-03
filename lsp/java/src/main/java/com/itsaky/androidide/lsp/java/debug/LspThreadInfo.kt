3c7e625f24d4ae76358eb485aa8ae4ef1acb71d9
itsaky-adfa <akashyadav@appdevforall.org>
2025-09-03 10:45:47 +0000

    ADFA-1420: include 'this' object in variables list (#332)
    Adds support for viewing and analyzing `this` instances when a breakpoint is hit in the IDE. This allows the users to view/modify instance and static fields of the current class when the current stack frame is a non-static and non-native method.

--- lsp/java/src/main/java/com/itsaky/androidide/lsp/java/debug/LspThreadInfo.kt
+++ lsp/java/src/main/java/com/itsaky/androidide/lsp/java/debug/LspThreadInfo.kt
@@ -22,142 +22,172 @@ import com.itsaky.androidide.lsp.debug.model.ThreadState as LspThreadState
 import com.itsaky.androidide.lsp.debug.model.Variable as LspVariable
 
 class JavaStackFrame(
-    val thread: ThreadReference,
-    val frame: StackFrame,
-    val location: Location = frame.location(),
-    val method: Method? = location.method(),
-    val sourceName: String = location.sourceName(),
-    val lineNumber: Long = location.lineNumber().toLong(),
+	val thread: ThreadReference,
+	val frame: StackFrame,
+	val location: Location = frame.location(),
+	val method: Method? = location.method(),
+	val sourceName: String = location.sourceName(),
+	val lineNumber: Long = location.lineNumber().toLong(),
 ) : LspStackFrame {
+	companion object {
+		private val logger = LoggerFactory.getLogger(JavaStackFrame::class.java)
+	}
 
-    companion object {
-        private val logger = LoggerFactory.getLogger(JavaStackFrame::class.java)
-    }
+	private lateinit var cachedVariables: List<AbstractJavaVariable<*>>
 
-    private lateinit var cachedVariables: List<JavaLocalVariable<*>>
+	override suspend fun descriptor() =
+		withContext(Dispatchers.IO) {
+			val method =
+				checkNotNull(method) {
+					"Method not found for location: $location"
+				}
 
-    override suspend fun descriptor() = withContext(Dispatchers.IO) {
-        val method = checkNotNull(method) {
-            "Method not found for location: $location"
-        }
+			StackFrameDescriptor(
+				method = method.name(),
+				methodSignature = method.signature(),
+				sourceFile = sourceName,
+				lineNumber = lineNumber,
+			)
+		}
 
-        StackFrameDescriptor(
-            method = method.name(),
-            methodSignature = method.signature(),
-            sourceFile = sourceName,
-            lineNumber = lineNumber
-        )
-    }
+	override suspend fun getVariables(): List<LspVariable<*>> {
+		if (!::cachedVariables.isInitialized) {
+			cachedVariables = JavaDebugAdapter.requireInstance().evalContext().evaluate(thread) {
+				if (method?.isOpaque == true) {
+					// non-concrete method
+					// does not have any variables
+					return@evaluate emptyList()
+				}
 
-    override suspend fun getVariables(): List<LspVariable<*>> {
-        if (!::cachedVariables.isInitialized) {
-            cachedVariables = JavaDebugAdapter.requireInstance().evalContext().evaluate(thread) {
-                if (method?.isOpaque == true) {
-                    // non-concrete method
-                    // does not have any variables
-                    return@evaluate emptyList()
-                }
+				return@evaluate thread
+					.frames()
+					.firstOrNull { frame ->
+						frame.location() == location
+					}?.run {
+						val variables = mutableListOf<AbstractJavaVariable<*>>()
+						val thisObject = this.thisObject()
+						if (thisObject != null) {
+							variables.add(
+								ThisVariable<Value>(
+									thread = thread,
+									ref = thisObject,
+									refType = thisObject.referenceType(),
+								),
+							)
+						}
 
-                return@evaluate thread.frames().firstOrNull { it.location() == location }
-                    ?.visibleVariables()
-                    ?.mapNotNull { variable ->
-                        if (variable.name().isBlank()) {
-                            // some opaque frames in core Android classes have empty variable names (like in ZygoteInit)
-                            return@mapNotNull null
-                        }
+						visibleVariables()
+							?.mapNotNull { variable ->
+								if (variable.name().isBlank()) {
+									// some opaque frames in core Android classes have empty variable names (like in ZygoteInit)
+									return@mapNotNull null
+								}
 
-                        try {
-                            JavaLocalVariable.forVariable(
-                                thread = thread,
-                                stackFrame = this,
-                                variable = variable,
-                                value = frame.getValue(variable)
-                            )
-                        } catch (err: Throwable) {
-                            logger.error("Failed to create variable wrapper for {}", variable.name(), err)
-                            null
-                        }
-                    }
-            } ?: emptyList()
-        }
+								try {
+									JavaLocalVariable.forVariable(
+										thread = thread,
+										stackFrame = this@JavaStackFrame,
+										variable = variable,
+										value = frame.getValue(variable),
+									)
+								} catch (err: Throwable) {
+									logger.error(
+										"Failed to create variable wrapper for {}",
+										variable.name(),
+										err,
+									)
+									null
+								}
+							}?.also { localVariables ->
+								variables.addAll(localVariables as List<AbstractJavaVariable<*>>)
+							}
 
-        return cachedVariables
-    }
+						variables
+					}
+			} ?: emptyList()
+		}
 
-    override suspend fun <Val : Value> setValue(variable: Variable<Val>, value: Val) =
-        withContext(Dispatchers.IO) {
-            variable as JavaLocalVariable
-            when (variable.kind) {
-                VariableKind.PRIMITIVE -> {
-                    check(value is PrimitiveValue) {
-                        "Value $value is not a primitive value"
-                    }
+		return cachedVariables
+	}
 
-                    variable as JavaPrimitiveVariable
-                    when (variable.primitiveKind) {
-                        PrimitiveKind.BOOLEAN -> variable.doSetValue(value.asBoolean())
-                        PrimitiveKind.BYTE -> variable.doSetValue(value.asByte())
-                        PrimitiveKind.CHAR -> variable.doSetValue(value.asChar())
-                        PrimitiveKind.SHORT -> variable.doSetValue(value.asShort())
-                        PrimitiveKind.INT -> variable.doSetValue(value.asInt())
-                        PrimitiveKind.LONG -> variable.doSetValue(value.asLong())
-                        PrimitiveKind.FLOAT -> variable.doSetValue(value.asFloat())
-                        PrimitiveKind.DOUBLE -> variable.doSetValue(value.asDouble())
-                    }
-                }
+	override suspend fun <Val : Value> setValue(
+		variable: Variable<Val>,
+		value: Val,
+	) = withContext(Dispatchers.IO) {
+		variable as JavaLocalVariable
+		when (variable.kind) {
+			VariableKind.PRIMITIVE -> {
+				check(value is PrimitiveValue) {
+					"Value $value is not a primitive value"
+				}
 
-                // TODO: Support other types of variable values
-                else -> throw IllegalStateException("Unsupported variable kind: ${variable.kind}")
-            }
-        }
+				variable as JavaPrimitiveVariable
+				when (variable.primitiveKind) {
+					PrimitiveKind.BOOLEAN -> variable.doSetValue(value.asBoolean())
+					PrimitiveKind.BYTE -> variable.doSetValue(value.asByte())
+					PrimitiveKind.CHAR -> variable.doSetValue(value.asChar())
+					PrimitiveKind.SHORT -> variable.doSetValue(value.asShort())
+					PrimitiveKind.INT -> variable.doSetValue(value.asInt())
+					PrimitiveKind.LONG -> variable.doSetValue(value.asLong())
+					PrimitiveKind.FLOAT -> variable.doSetValue(value.asFloat())
+					PrimitiveKind.DOUBLE -> variable.doSetValue(value.asDouble())
+				}
+			}
+
+			// TODO: Support other types of variable values
+			else -> throw IllegalStateException("Unsupported variable kind: ${variable.kind}")
+		}
+	}
 }
 
 internal class LspThreadInfo(
-    val thread: ThreadInfo,
-    val frames: List<StackFrame> = thread.frames(),
+	val thread: ThreadInfo,
+	val frames: List<StackFrame> = thread.frames(),
 ) : LspThreadInfo {
+	companion object {
+		private val logger = LoggerFactory.getLogger(LspThreadInfo::class.java)
+	}
 
-    companion object {
-        private val logger = LoggerFactory.getLogger(LspThreadInfo::class.java)
-    }
+	private lateinit var cachedFrames: List<JavaStackFrame>
 
-    private lateinit var cachedFrames: List<JavaStackFrame>
+	override suspend fun descriptor(): ThreadDescriptor =
+		withContext(Dispatchers.IO) {
+			val thread = thread.thread
+			val group = thread.threadGroup()
 
-    override suspend fun descriptor(): ThreadDescriptor = withContext(Dispatchers.IO) {
-        val thread = thread.thread
-        val group = thread.threadGroup()
+			ThreadDescriptor(
+				id = thread.uniqueID().toString(),
+				name = thread.name(),
+				group = group.name(),
+				state = threadStateOf(thread.status()),
+			)
+		}
 
-        ThreadDescriptor(
-            id = thread.uniqueID().toString(),
-            name = thread.name(),
-            group = group.name(),
-            state = threadStateOf(thread.status())
-        )
-    }
+	override suspend fun getFrames(): List<JavaStackFrame> {
+		if (!::cachedFrames.isInitialized) {
+			cachedFrames =
+				JavaDebugAdapter.requireInstance().evalContext().evaluate(thread.thread) {
+					frames.map { frame ->
+						JavaStackFrame(
+							thread = thread.thread,
+							frame = frame,
+						)
+					}
+				} ?: emptyList()
+		}
 
-    override suspend fun getFrames(): List<JavaStackFrame> {
-        if (!::cachedFrames.isInitialized) {
-            cachedFrames = JavaDebugAdapter.requireInstance().evalContext().evaluate(thread.thread) {
-                frames.map { frame ->
-                    JavaStackFrame(
-                        thread = thread.thread,
-                        frame = frame,
-                    )
-                }
-            } ?: emptyList()
-        }
-
-        return cachedFrames
-    }
+		return cachedFrames
+	}
 }
 
-private fun threadStateOf(state: Int) = when (state) {
-    ThreadReference.THREAD_STATUS_UNKNOWN -> LspThreadState.UNKNOWN
-    ThreadReference.THREAD_STATUS_ZOMBIE -> LspThreadState.ZOMBIE
-    ThreadReference.THREAD_STATUS_RUNNING -> LspThreadState.RUNNING
-    ThreadReference.THREAD_STATUS_SLEEPING -> LspThreadState.SLEEPING
-    ThreadReference.THREAD_STATUS_MONITOR -> LspThreadState.MONITOR
-    ThreadReference.THREAD_STATUS_WAIT -> LspThreadState.WAITING
-    ThreadReference.THREAD_STATUS_NOT_STARTED -> LspThreadState.NOT_STARTED
-    else -> ThreadState.UNKNOWN
-}
+private fun threadStateOf(state: Int) =
+	when (state) {
+		ThreadReference.THREAD_STATUS_UNKNOWN -> LspThreadState.UNKNOWN
+		ThreadReference.THREAD_STATUS_ZOMBIE -> LspThreadState.ZOMBIE
+		ThreadReference.THREAD_STATUS_RUNNING -> LspThreadState.RUNNING
+		ThreadReference.THREAD_STATUS_SLEEPING -> LspThreadState.SLEEPING
+		ThreadReference.THREAD_STATUS_MONITOR -> LspThreadState.MONITOR
+		ThreadReference.THREAD_STATUS_WAIT -> LspThreadState.WAITING
+		ThreadReference.THREAD_STATUS_NOT_STARTED -> LspThreadState.NOT_STARTED
+		else -> ThreadState.UNKNOWN
+	}
