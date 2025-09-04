6b5af19a31ba75324caf904bf3d0c3e76aaf9ecc
Dara Abijo <oluwadara.abijo@appdevforall.org>
2025-09-04 09:19:16 +0100

    ADFA-1197: Set tooltips on structure view items (#352)
    feat(ADFA-1197): Set tooltips on structure view items

--- layouteditor/src/main/java/org/appdevforall/codeonthego/layouteditor/activities/EditorActivity.kt
+++ layouteditor/src/main/java/org/appdevforall/codeonthego/layouteditor/activities/EditorActivity.kt
@@ -1,6 +1,7 @@
 package org.appdevforall.codeonthego.layouteditor.activities
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
 import android.net.Uri
@@ -9,6 +10,7 @@ import android.os.Handler
 import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.util.TypedValue
 import android.view.Menu
 import android.view.MenuItem
@@ -33,6 +35,14 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import com.itsaky.androidide.idetooltips.TooltipCategory
+import com.itsaky.androidide.idetooltips.TooltipManager.getTooltip
+import com.itsaky.androidide.idetooltips.TooltipManager.showIDETooltip
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.adfa.constants.CONTENT_KEY
+import org.adfa.constants.CONTENT_TITLE_KEY
 import org.appdevforall.codeonthego.layouteditor.BaseActivity
 import org.appdevforall.codeonthego.layouteditor.LayoutFile
 import org.appdevforall.codeonthego.layouteditor.ProjectFile
@@ -255,9 +265,46 @@ class EditorActivity : BaseActivity() {
     private fun setupStructureView() {
         binding.editorLayout.setStructureView(binding.structureView)
 
-        binding.structureView.onItemClickListener = {
-            binding.editorLayout.showDefinedAttributes(it)
-            drawerLayout.closeDrawer(GravityCompat.END)
+        binding.structureView.apply {
+            onItemClickListener = {
+                binding.editorLayout.showDefinedAttributes(it)
+                drawerLayout.closeDrawer(GravityCompat.END)
+            }
+            onItemLongClickListener = { view ->
+                showTooltip(
+                    context = this@EditorActivity,
+                    anchorView = view,
+                    tag = view.javaClass.superclass.name
+                )
+            }
+        }
+    }
+
+    private fun showTooltip(context: Context, anchorView: View, tag: String) {
+        CoroutineScope(Dispatchers.Main).launch {
+            val tooltipItem = getTooltip(
+                context,
+                TooltipCategory.CATEGORY_JAVA,
+                tag,
+            )
+            if (tooltipItem != null) {
+                showIDETooltip(
+                    context = context,
+                    anchorView = anchorView,
+                    level = 0,
+                    tooltipItem = tooltipItem,
+                    onHelpLinkClicked = { context, url, title ->
+                        val intent =
+                            Intent(context, HelpActivity::class.java).apply {
+                                putExtra(CONTENT_KEY, url)
+                                putExtra(CONTENT_TITLE_KEY, title)
+                            }
+                        context.startActivity(intent)
+                    }
+                )
+            } else {
+                Log.e("TooltipManager", "Tooltip item $tooltipItem is null")
+            }
         }
     }
 
