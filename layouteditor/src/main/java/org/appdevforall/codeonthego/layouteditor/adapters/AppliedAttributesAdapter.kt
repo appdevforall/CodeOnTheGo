5a2aae5ebf18b8398245877db9c8e41c01840942
Oluwadara Abijo <oluwadara.abijo@appdevforall.org>
2025-09-05 19:55:45 +0100

    feat(ADFA-1196): Prepend tooltip tag with string

--- layouteditor/src/main/java/org/appdevforall/codeonthego/layouteditor/adapters/AppliedAttributesAdapter.kt
+++ layouteditor/src/main/java/org/appdevforall/codeonthego/layouteditor/adapters/AppliedAttributesAdapter.kt
@@ -11,50 +11,56 @@ import org.appdevforall.codeonthego.layouteditor.databinding.ShowAttributeItemBi
 import org.appdevforall.codeonthego.layouteditor.utils.Constants
 
 class AppliedAttributesAdapter(
-  private val attrs: List<HashMap<String, Any>>,
-  private val values: List<String>
+    private val attrs: List<HashMap<String, Any>>,
+    private val values: List<String>
 ) : RecyclerView.Adapter<AppliedAttributesAdapter.VH>() {
 
-  var onClick: (Int) -> Unit = {}
-  var onRemoveButtonClick: (Int) -> Unit = {}
+    var onClick: (Int) -> Unit = {}
+    var onRemoveButtonClick: (Int) -> Unit = {}
 
-  class VH(var binding: ShowAttributeItemBinding) : RecyclerView.ViewHolder(
-    binding.root
-  ) {
-    var btnRemove = binding.btnRemoveAttribute
-    var attributeName = binding.attributeName
-    var attributeValue = binding.attributeValue
-  }
-
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
-    return VH(
-      ShowAttributeItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-    )
-  }
-
-  override fun onBindViewHolder(holder: VH, position: Int) {
-    holder.attributeName.text = attrs[position]["name"].toString()
-    holder.attributeValue.text = values[position]
-
-    TooltipCompat.setTooltipText(holder.btnRemove, "Remove")
-
-    if (attrs[position].containsKey(Constants.KEY_CAN_DELETE)) {
-      holder.btnRemove.visibility = View.GONE
+    class VH(var binding: ShowAttributeItemBinding) : RecyclerView.ViewHolder(
+        binding.root
+    ) {
+        var btnRemove = binding.btnRemoveAttribute
+        var attributeName = binding.attributeName
+        var attributeValue = binding.attributeValue
     }
 
-      holder.binding.root.apply {
-          setOnClickListener { onClick(position) }
-          displayTooltipOnLongPress(
-              context = this.context,
-              anchorView = this,
-              tooltipCategory = TooltipCategory.CATEGORY_XML,
-              tooltipTag = attrs[position]["name"].toString().substringAfterLast(":")
-          )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        return VH(
+            ShowAttributeItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        val attributeName = attrs[position]["name"].toString()
+        holder.attributeName.text = attributeName
+        holder.attributeValue.text = values[position]
+
+        TooltipCompat.setTooltipText(holder.btnRemove, "Remove")
+
+        if (attrs[position].containsKey(Constants.KEY_CAN_DELETE)) {
+            holder.btnRemove.visibility = View.GONE
         }
-      holder.btnRemove.setOnClickListener { onRemoveButtonClick(position) }
+
+        holder.binding.root.apply {
+            setOnClickListener { onClick(position) }
+            displayTooltipOnLongPress(
+                context = this.context,
+                anchorView = this,
+                tooltipCategory = TooltipCategory.CATEGORY_XML,
+                tooltipTag = createTooltipTag(attributeName)
+            )
+        }
+        holder.btnRemove.setOnClickListener { onRemoveButtonClick(position) }
     }
 
-  override fun getItemCount(): Int {
-    return attrs.size
-  }
+    private fun createTooltipTag(name: String): String {
+        val prefix = "xml.attr."
+        return "$prefix${name.substringAfterLast(":")}"
+    }
+
+    override fun getItemCount(): Int {
+        return attrs.size
+    }
 }
