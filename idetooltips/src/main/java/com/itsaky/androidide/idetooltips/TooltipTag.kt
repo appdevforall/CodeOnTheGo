2d29a36994a6e1aa089c180ce2354c7f43df391d
John Trujillo <johnandrestrujillo@gmail.com>
2025-09-08 13:49:28 -0500

    Merge conflicts

--- idetooltips/src/main/java/com/itsaky/androidide/idetooltips/TooltipTag.kt
+++ idetooltips/src/main/java/com/itsaky/androidide/idetooltips/TooltipTag.kt
@@ -68,7 +68,6 @@ object TooltipTag {
 	const val PROJECT_NEW = "project.new"
 	const val PROJECT_RECENT_TOP = "project.recent.top"
 	const val PROJECT_RECENT_RENAME = "project.recent.rename"
-	const val PROJECT_RENAME_DIALOG = "project.rename.dialog"
 	const val PROJECT_OPEN_FOLDER = "project.open.folder"
 
     const val PROJECT_ITEM_COPYPATH = "project.item.copypath"
