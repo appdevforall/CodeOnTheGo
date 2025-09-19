appdevforall/CodeOnTheGo

commit 3c20fb98f14935a8c9c6288a4140af104c35b84c

ADFA-1367: Set tooltips in editor toolbar (#333)
* feat(ADFA-1367): Add tooltip tags to editor toolbar action items

* feat(ADFA-1367): Set long click listener on editor toolbar action items

* feat(ADFA-1367): Show tooltips when editor toolbar action items are long-pressed

* feat(ADFA-1367): Show tooltip when editor hamburger is long-pressed

* feat(ADFA-1367): Set tooltip on project overview

* feat(ADFA-1367): Set tooltip on project status

* refactor(ADFA-1367): Move code to appropriate function

* refactor(adfa-1367): Modify tooltip function

* feat(ADFA-1367): Override tooltip tag

* fix(ADFA-1367): Enable long-press on spannable text

app/src/main/java/com/itsaky/androidide/actions/build/QuickRunAction.kt
@@ -23,7 +23,13 @@ import android.graphics.PorterDuff
 import android.graphics.PorterDuffColorFilter
 import com.itsaky.androidide.actions.ActionData
 import com.itsaky.androidide.actions.getContext
+import com.itsaky.androidide.activities.editor.EditorHandlerActivity
+import com.itsaky.androidide.idetooltips.TooltipTag
+import com.itsaky.androidide.projects.api.AndroidModule
+import com.itsaky.androidide.projects.builder.BuildService
 import com.itsaky.androidide.resources.R
+import com.itsaky.androidide.tooling.api.messages.TaskExecutionMessage
+import com.itsaky.androidide.tooling.api.models.BasicAndroidVariantMetadata
 import com.itsaky.androidide.utils.resolveAttr
 
 /**
@@ -34,31 +40,41 @@ import com.itsaky.androidide.utils.resolveAttr
  *
  * @author Akash Yadav
  */
-class QuickRunAction(
-	context: Context,
-	override val order: Int,
-) : AbstractRunAction(
-		context = context,
-		labelRes = R.string.quick_run_debug,
-		iconRes = R.drawable.ic_run_outline,
-	) {
-	override val id: String = ID
+class QuickRunAction(context: Context, override val order: Int) :
+    AbstractRunAction(
+        context = context,
+        labelRes = R.string.quick_run_debug,
+        iconRes = R.drawable.ic_run_outline
+    ) {
 
-	companion object {
-		const val ID = "ide.editor.build.quickRun"
-	}
+    override val id: String = ID
+    companion object {
+        const val ID = "ide.editor.build.quickRun"
+    }
+    override fun retrieveTooltipTag(isReadOnlyContext: Boolean): String = TooltipTag.EDITOR_TOOLBAR_QUICK_RUN
 
-	override fun createColorFilter(data: ActionData): ColorFilter? =
-		data.getContext()?.let {
-			PorterDuffColorFilter(
-				it.resolveAttr(
-					if (data.getActivity().isBuildInProgress()) {
-						R.attr.colorError
-					} else {
-						R.attr.colorSuccess
-					},
-				),
-				PorterDuff.Mode.SRC_ATOP,
-			)
-		}
+    override fun onCreateTaskExecMessage(
+        data: ActionData,
+        module: AndroidModule,
+        variant: BasicAndroidVariantMetadata,
+        buildService: BuildService,
+        activity: EditorHandlerActivity
+    ): TaskExecutionMessage {
+        val taskName = "${module.path}:${variant.mainArtifact.assembleTaskName}"
+        log.info("Running task '{}' to assemble variant '{}' of project '{}'", taskName, variant.name, module.path)
+
+        return TaskExecutionMessage(tasks = listOf(taskName))
+    }
+
+    override fun createColorFilter(data: ActionData): ColorFilter? {
+        return data.getContext()?.let {
+            PorterDuffColorFilter(
+                it.resolveAttr(
+                    if (data.getActivity().isBuildInProgress())
+                        R.attr.colorError
+                    else R.attr.colorSuccess
+                ), PorterDuff.Mode.SRC_ATOP
+            )
+        }
+    }
 }

app/src/main/java/com/itsaky/androidide/actions/etc/LaunchAppAction.kt
@@ -25,6 +25,7 @@ import com.itsaky.androidide.actions.ActionData
 import com.itsaky.androidide.actions.EditorActivityAction
 import com.itsaky.androidide.actions.markInvisible
 import com.itsaky.androidide.actions.openApplicationModuleChooser
+import com.itsaky.androidide.idetooltips.TooltipTag.EDITOR_TOOLBAR_LAUNCH_APP
 import com.itsaky.androidide.projects.IProjectManager
 import com.itsaky.androidide.utils.IntentUtils
 import com.itsaky.androidide.utils.flashError
@@ -36,68 +37,63 @@ import org.slf4j.LoggerFactory
  *
  * @author Akash Yadav
  */
-class LaunchAppAction(
-	context: Context,
-	override val order: Int,
-) : EditorActivityAction() {
-	override val id: String = "ide.editor.launchInstalledApp"
-	override var requiresUIThread: Boolean = true
+class LaunchAppAction(context: Context, override val order: Int) : EditorActivityAction() {
 
-	init {
-		label = context.getString(R.string.title_launch_app)
-		icon = ContextCompat.getDrawable(context, R.drawable.ic_open_external)
-	}
+    override val id: String = "ide.editor.launchInstalledApp"
+    override var requiresUIThread: Boolean = true
+    override fun retrieveTooltipTag(isReadOnlyContext: Boolean) = EDITOR_TOOLBAR_LAUNCH_APP
 
-	companion object {
-		private val log = LoggerFactory.getLogger(LaunchAppAction::class.java)
-	}
+    init {
+        label = context.getString(R.string.title_launch_app)
+        icon = ContextCompat.getDrawable(context, R.drawable.ic_open_external)
+    }
 
-	override fun prepare(data: ActionData) {
-		super.prepare(data)
-		data.getActivity() ?: run {
-			markInvisible()
-			return
-		}
+    companion object {
+        private val log = LoggerFactory.getLogger(LaunchAppAction::class.java)
+    }
 
-		visible = true
+    override fun prepare(data: ActionData) {
+        super.prepare(data)
+        data.getActivity() ?: run {
+            markInvisible()
+            return
+        }
 
-		val projectManager = IProjectManager.getInstance()
-		enabled = projectManager.getAndroidAppModules().isNotEmpty()
-	}
+        visible = true
 
-	override suspend fun execAction(data: ActionData) {
-		openApplicationModuleChooser(data) { app ->
-			val variant = app.getSelectedVariant()
+        val projectManager = IProjectManager.getInstance()
+        enabled = projectManager.getAndroidAppModules().isNotEmpty()
+    }
 
-			log.debug("Selected variant: {}", variant?.name)
+    override suspend fun execAction(data: ActionData) {
+        openApplicationModuleChooser(data) { app ->
+            val variant = app.getSelectedVariant()
 
-			if (variant == null) {
-				flashError(R.string.err_selected_variant_not_found)
-				return@openApplicationModuleChooser
-			}
+            log.debug("Selected variant: {}", variant?.name)
 
-			val applicationId = variant.mainArtifact.applicationId
-			if (applicationId == null) {
-				log.error("Unable to launch application. variant.mainArtifact.applicationId is null")
-				flashError(R.string.err_cannot_determine_package)
-				return@openApplicationModuleChooser
-			}
+            if (variant == null) {
+                flashError(R.string.err_selected_variant_not_found)
+                return@openApplicationModuleChooser
+            }
 
-			log.info("Launching application: {}", applicationId)
+            val applicationId = variant.mainArtifact.applicationId
+            if (applicationId == null) {
+                log.error("Unable to launch application. variant.mainArtifact.applicationId is null")
+                flashError(R.string.err_cannot_determine_package)
+                return@openApplicationModuleChooser
+            }
 
-			val activity = data.requireActivity()
-			activity.activityScope.launch {
-				IntentUtils.launchApp(
-					context = activity,
-					packageName = applicationId,
-					logError = false,
-				)
-			}
-		}
-	}
+            log.info("Launching application: {}", applicationId)
 
-	override fun getShowAsActionFlags(data: ActionData): Int {
-		// prefer showing this in the overflow menu
-		return MenuItem.SHOW_AS_ACTION_IF_ROOM
-	}
+            val activity = data.requireActivity()
+            activity.activityScope.launch {
+                IntentUtils.launchApp(activity, applicationId, logError = false)
+            }
+        }
+    }
+
+    override fun getShowAsActionFlags(data: ActionData): Int {
+        // prefer showing this in the overflow menu
+        return MenuItem.SHOW_AS_ACTION_IF_ROOM
+    }
 }

app/src/main/java/com/itsaky/androidide/actions/build/DebugAction.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.itsaky.androidide.actions.ActionData
+import com.itsaky.androidide.idetooltips.TooltipTag
 import com.itsaky.androidide.fragments.debug.DebuggerFragment
 import com.itsaky.androidide.lsp.java.debug.JdwpOptions
 import com.itsaky.androidide.resources.R
@@ -26,6 +27,7 @@ class DebugAction(
 		iconRes = R.drawable.ic_db_startdebugger,
 	) {
 	override val id = ID
+    override fun retrieveTooltipTag(isReadOnlyContext: Boolean) = TooltipTag.EDITOR_TOOLBAR_DEBUG
 
 	companion object {
 		const val ID = "ide.editor.build.debug"

app/src/main/java/com/itsaky/androidide/actions/build/ProjectSyncAction.kt
@@ -21,6 +21,7 @@ import android.content.Context
 import androidx.core.content.ContextCompat
 import com.itsaky.androidide.actions.ActionData
 import com.itsaky.androidide.actions.BaseBuildAction
+import com.itsaky.androidide.idetooltips.TooltipTag
 import com.itsaky.androidide.resources.R
 import com.itsaky.androidide.resources.R.string
 
@@ -42,6 +43,8 @@ class ProjectSyncAction(context: Context, override val order: Int) : BaseBuildAc
     label = context.getString(string.title_sync_project)
     icon = ContextCompat.getDrawable(context, R.drawable.ic_sync)
   }
+
+    override fun retrieveTooltipTag(isReadOnlyContext: Boolean): String = TooltipTag.EDITOR_TOOLBAR_SYNC
 
   override suspend fun execAction(data: ActionData): Any {
     return data.requireActivity().saveAll(requestSync = false)

app/src/main/java/com/itsaky/androidide/actions/build/RunTasksAction.kt
@@ -22,11 +22,14 @@ import androidx.core.content.ContextCompat
 import com.itsaky.androidide.actions.ActionData
 import com.itsaky.androidide.actions.BaseBuildAction
 import com.itsaky.androidide.fragments.RunTasksDialogFragment
+import com.itsaky.androidide.idetooltips.TooltipCategory
+import com.itsaky.androidide.idetooltips.TooltipTag
 import com.itsaky.androidide.resources.R
 
 /** @author Akash Yadav */
 class RunTasksAction(context: Context, override val order: Int) : BaseBuildAction() {
   override val id: String = ID
+    override fun retrieveTooltipTag(isReadOnlyContext: Boolean): String = TooltipTag.EDITOR_TOOLBAR_RUN_TASKS
   private var dialog: RunTasksDialogFragment? = null
 
   companion object {

app/src/main/java/com/itsaky/androidide/actions/etc/FindAction.kt
@@ -22,6 +22,7 @@ import androidx.core.content.ContextCompat
 import com.itsaky.androidide.actions.ActionData
 import com.itsaky.androidide.actions.EditorActivityAction
 import com.itsaky.androidide.actions.build.AbstractCancellableRunAction.Companion.isBuildInProgress
+import com.itsaky.androidide.idetooltips.TooltipTag
 import com.itsaky.androidide.resources.R
 
 /** @author Akash Yadav */
@@ -29,7 +30,10 @@ class FindAction() : EditorActivityAction() {
 
     override var requiresUIThread: Boolean = true
     override var order: Int = 0
-
+    override fun retrieveTooltipTag(isReadOnlyContext: Boolean): String {
+        return TooltipTag.EDITOR_TOOLBAR_FIND
+    }
+    
     constructor(context: Context, order: Int) : this() {
         this.label = context.getString(R.string.menu_find)
         this.icon = ContextCompat.getDrawable(context, R.drawable.ic_search)

app/src/main/java/com/itsaky/androidide/actions/etc/FindInProjectAction.kt
@@ -23,6 +23,7 @@ import com.itsaky.androidide.actions.ActionData
 import com.itsaky.androidide.actions.ActionItem
 import com.itsaky.androidide.actions.EditorActivityAction
 import com.itsaky.androidide.actions.markInvisible
+import com.itsaky.androidide.idetooltips.TooltipTag
 import com.itsaky.androidide.projects.IProjectManager
 import com.itsaky.androidide.resources.R
 
@@ -32,6 +33,7 @@ class FindInProjectAction() : EditorActivityAction() {
   override var requiresUIThread: Boolean = true
   override var order: Int = 0
   override var location: ActionItem.Location = ActionItem.Location.EDITOR_FIND_ACTION_MENU
+    override fun retrieveTooltipTag(isReadOnlyContext: Boolean): String = TooltipTag.EDITOR_TOOLBAR_FIND_IN_PROJECT
 
   constructor(context: Context, order: Int) : this() {
     this.label = context.getString(R.string.menu_find_project)

commit 328f80ef21b86cf3e871f1a18638edb61f776981

ADFA-1544: bring back debugger (#397)
* fix: bring back debugger (debugger features were broken in commit 3c20fb98f14935a8c9c6288a4140af104c35b84c).
* fix: ensure debugeePackageFlow is observed in background

---------

Signed-off-by: Akash Yadav <akashyadav@appdevforall.org>

app/src/main/java/com/itsaky/androidide/actions/build/QuickRunAction.kt
@@ -41,40 +41,30 @@ import com.itsaky.androidide.utils.resolveAttr
  * @author Akash Yadav
  */
 class QuickRunAction(context: Context, override val order: Int) :
-    AbstractRunAction(
-        context = context,
-        labelRes = R.string.quick_run_debug,
-        iconRes = R.drawable.ic_run_outline
-    ) {
+	AbstractRunAction(
+		context = context,
+		labelRes = R.string.quick_run_debug,
+		iconRes = R.drawable.ic_run_outline
+	) {
 
-    override val id: String = ID
-    companion object {
-        const val ID = "ide.editor.build.quickRun"
-    }
-    override fun retrieveTooltipTag(isReadOnlyContext: Boolean): String = TooltipTag.EDITOR_TOOLBAR_QUICK_RUN
+	override val id: String = ID
 
-    override fun onCreateTaskExecMessage(
-        data: ActionData,
-        module: AndroidModule,
-        variant: BasicAndroidVariantMetadata,
-        buildService: BuildService,
-        activity: EditorHandlerActivity
-    ): TaskExecutionMessage {
-        val taskName = "${module.path}:${variant.mainArtifact.assembleTaskName}"
-        log.info("Running task '{}' to assemble variant '{}' of project '{}'", taskName, variant.name, module.path)
+	companion object {
+		const val ID = "ide.editor.build.quickRun"
+	}
 
-        return TaskExecutionMessage(tasks = listOf(taskName))
-    }
+	override fun retrieveTooltipTag(isReadOnlyContext: Boolean): String =
+		TooltipTag.EDITOR_TOOLBAR_QUICK_RUN
 
-    override fun createColorFilter(data: ActionData): ColorFilter? {
-        return data.getContext()?.let {
-            PorterDuffColorFilter(
-                it.resolveAttr(
-                    if (data.getActivity().isBuildInProgress())
-                        R.attr.colorError
-                    else R.attr.colorSuccess
-                ), PorterDuff.Mode.SRC_ATOP
-            )
-        }
-    }
+	override fun createColorFilter(data: ActionData): ColorFilter? {
+		return data.getContext()?.let {
+			PorterDuffColorFilter(
+				it.resolveAttr(
+					if (data.getActivity().isBuildInProgress())
+						R.attr.colorError
+					else R.attr.colorSuccess
+				), PorterDuff.Mode.SRC_ATOP
+			)
+		}
+	}
 }

app/src/main/java/com/itsaky/androidide/actions/etc/LaunchAppAction.kt
@@ -87,7 +87,11 @@ class LaunchAppAction(context: Context, override val order: Int) : EditorActivit
 
             val activity = data.requireActivity()
             activity.activityScope.launch {
-                IntentUtils.launchApp(activity, applicationId, logError = false)
+                IntentUtils.launchApp(
+					context = activity,
+					packageName = applicationId,
+					logError = false
+				)
             }
         }
     }

app/src/main/java/com/itsaky/androidide/activities/editor/BaseEditorActivity.kt
@@ -32,10 +32,8 @@ import android.os.Process
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.SpannableStringBuilder
-import android.text.Spanned
 import android.text.TextUtils
 import android.text.method.LinkMovementMethod
-import android.text.style.ClickableSpan
 import android.text.style.LeadingMarginSpan
 import android.view.GestureDetector
 import android.util.Log
@@ -50,7 +48,6 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.activity.viewModels
 import androidx.annotation.GravityInt
-import androidx.annotation.StringRes
 import androidx.annotation.UiThread
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.collection.MutableIntIntMap
@@ -59,7 +56,6 @@ import androidx.core.view.GravityCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
 import androidx.core.view.updatePadding
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -88,7 +84,6 @@ import com.itsaky.androidide.databinding.ActivityEditorBinding
 import com.itsaky.androidide.databinding.ContentEditorBinding
 import com.itsaky.androidide.databinding.LayoutDiagnosticInfoBinding
 import com.itsaky.androidide.events.InstallationResultEvent
-import com.itsaky.androidide.fragments.SearchResultFragment
 import com.itsaky.androidide.fragments.sidebar.EditorSidebarFragment
 import com.itsaky.androidide.fragments.sidebar.FileTreeFragment
 import com.itsaky.androidide.handlers.EditorActivityLifecyclerObserver
@@ -510,6 +505,8 @@ abstract class BaseEditorActivity :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+		Shizuku.addBinderReceivedListener(shizukuBinderReceivedListener)
+
         this.optionsMenuInvalidator = Runnable { super.invalidateOptionsMenu() }
 
         registerLanguageServers()
@@ -766,7 +763,7 @@ abstract class BaseEditorActivity :
             hideBottomSheet()
         })
 
-        showSearchResults()
+		bottomSheetViewModel.setSheetState(currentTab = BottomSheetViewModel.TAB_SEARCH_RESULT)
         doDismissSearchProgress()
     }
 
@@ -779,53 +776,34 @@ abstract class BaseEditorActivity :
     }
 
     open fun hideBottomSheet() {
-        if (editorBottomSheet?.state != BottomSheetBehavior.STATE_COLLAPSED) {
-            editorBottomSheet?.state = BottomSheetBehavior.STATE_COLLAPSED
-        }
+		bottomSheetViewModel.setSheetState(sheetState = BottomSheetBehavior.STATE_COLLAPSED)
     }
 
-    open fun showSearchResults() = showBottomSheetFragment(SearchResultFragment::class.java)
+	private fun updateBottomSheetState(state: BottomSheetViewModel.SheetState = BottomSheetViewModel.SheetState.EMPTY) {
+		log.debug("updateSheetState: {}", state)
+		content.bottomSheet.setCurrentTab(state.currentTab)
+		if (editorBottomSheet?.state != state.sheetState) {
+			editorBottomSheet?.state = state.sheetState
+		}
+	}
 
-    open fun showBottomSheetFragment(
-        fragmentClass: Class<out Fragment>,
-        sheetState: Int = BottomSheetBehavior.STATE_EXPANDED
-    ) {
-        showAndGetBottomSheetFragment(fragmentClass, sheetState)
-    }
 
-    open fun <T: Fragment> showAndGetBottomSheetFragment(
-        fragmentClass: Class<T>,
-        sheetState: Int = BottomSheetBehavior.STATE_EXPANDED
-    ): T? = content.bottomSheet.run {
-        val index = pagerAdapter.findIndexOfFragmentByClass(fragmentClass)
-        val fragment = pagerAdapter.getFragmentAtIndex<T>(index) ?: let {
-            log.error("Failed to get bottom sheet fragment at index: {}", index)
-            return@run null
-        }
-
-        if (index >= 0 && index < binding.tabs.tabCount) {
-            if (editorBottomSheet?.state != sheetState) {
-                editorBottomSheet?.state = sheetState
-            }
-            binding.tabs.getTabAt(index)?.select()
-        }
-
-        fragment
-    }
-
-    open fun handleDiagnosticsResultVisibility(errorVisible: Boolean) {
-        content.bottomSheet.handleDiagnosticsResultVisibility(errorVisible)
-    }
+	open fun handleDiagnosticsResultVisibility(errorVisible: Boolean) {
+		content.bottomSheet.handleDiagnosticsResultVisibility(errorVisible)
+	}
 
     open fun handleSearchResultVisibility(errorVisible: Boolean) {
         content.bottomSheet.handleSearchResultVisibility(errorVisible)
     }
 
     open fun showFirstBuildNotice() {
-        newMaterialDialogBuilder(this).setPositiveButton(android.R.string.ok, null)
-            .setTitle(string.title_first_build).setMessage(string.msg_first_build)
+        newMaterialDialogBuilder(this)
+			.setPositiveButton(android.R.string.ok, null)
+            .setTitle(string.title_first_build)
+			.setMessage(string.msg_first_build)
             .setCancelable(false)
-            .create().show()
+            .create()
+			.show()
     }
 
     open fun getFileTreeFragment(): FileTreeFragment? {
@@ -918,21 +896,15 @@ abstract class BaseEditorActivity :
         invalidateOptionsMenu()
     }
 
-    private fun setupViews() {
-        lifecycleScope.launch {
-			// debugger state updates which does no affect the UI must be
-			// observed in the CREATED state in order to ensure that we get
-			// notified about the updates even when the IDE is in the background
-			//
-			// if you need to observe state for UI updates, please add a new
-			// repeatOnLifecycle call with Lifecycle.State.STARTED
+	private fun setupViews() {
+		lifecycleScope.launch {
 			repeatOnLifecycle(Lifecycle.State.CREATED) {
 				launch {
+					// should be active from CREATED through DESTROYED because
+					// debugger connection updates can happen in the background
+					// which won't be reported if we use Lifecycle.State.STARTED
 					debuggerViewModel.connectionState.collectLatest { state ->
-						if (state == DebuggerConnectionState.ATTACHED) {
-							ensureDebuggerServiceBound()
-						}
-						postStopDebuggerServiceIfNotConnected()
+						onDebuggerConnectionStateChanged(state)
 					}
 				}
 
@@ -942,7 +914,17 @@ abstract class BaseEditorActivity :
 					}
 				}
 			}
-        }
+		}
+
+		lifecycleScope.launch {
+			repeatOnLifecycle(Lifecycle.State.STARTED) {
+				launch {
+					bottomSheetViewModel.sheetState.collectLatest { state ->
+						updateBottomSheetState(state = state)
+					}
+				}
+			}
+		}
 
         editorViewModel._isBuildInProgress.observe(this) { onUpdateProgressBarVisibility() }
         editorViewModel._isInitializing.observe(this) { onUpdateProgressBarVisibility() }
@@ -972,11 +954,11 @@ abstract class BaseEditorActivity :
 
         if (!app.prefManager.getBoolean(
                 KEY_BOTTOM_SHEET_SHOWN
-            ) && editorBottomSheet?.state != BottomSheetBehavior.STATE_EXPANDED
+            ) && bottomSheetViewModel.sheetBehaviorState != BottomSheetBehavior.STATE_EXPANDED
         ) {
-            editorBottomSheet?.state = BottomSheetBehavior.STATE_EXPANDED
+            bottomSheetViewModel.setSheetState(BottomSheetBehavior.STATE_EXPANDED)
             ThreadUtils.runOnUiThreadDelayed({
-                editorBottomSheet?.state = BottomSheetBehavior.STATE_COLLAPSED
+				bottomSheetViewModel.setSheetState(BottomSheetBehavior.STATE_COLLAPSED)
                 app.prefManager.putBoolean(KEY_BOTTOM_SHEET_SHOWN, true)
             }, 1500)
         }
@@ -998,13 +980,37 @@ abstract class BaseEditorActivity :
         }
     }
 
-    private fun setupNoEditorView() {
-        content.noEditorLayout.setOnLongClickListener {
-            showTooltip(
-                tag = TooltipTag.EDITOR_PROJECT_OVERVIEW
-            )
-            true
-        }
+	protected open fun onDebuggerConnectionStateChanged(state: DebuggerConnectionState) {
+		log.debug("onDebuggerConnectionStateChanged: {}", state)
+		if (state == DebuggerConnectionState.ATTACHED) {
+			ensureDebuggerServiceBound()
+		}
+
+		debuggerService?.setOverlayVisibility(state >= DebuggerConnectionState.ATTACHED)
+		if (state == DebuggerConnectionState.ATTACHED) {
+			// if a VM was just attached, make sure the debugger fragment is visible
+			bottomSheetViewModel.setSheetState(
+				sheetState = BottomSheetBehavior.STATE_HALF_EXPANDED,
+				currentTab = BottomSheetViewModel.TAB_DEBUGGER,
+			)
+		}
+
+		if (state == DebuggerConnectionState.AWAITING_BREAKPOINT) {
+			// breakpoint hit, ensure IDE is in foreground
+			debuggerViewModel.switchToIde(context = this)
+		}
+
+		postStopDebuggerServiceIfNotConnected()
+	}
+
+
+	private fun setupNoEditorView() {
+		content.noEditorLayout.setOnLongClickListener {
+			showTooltip(
+				tag = TooltipTag.EDITOR_PROJECT_OVERVIEW
+			)
+			true
+		}
         content.noEditorSummary.movementMethod = LinkMovementMethod()
         val sb = SpannableStringBuilder()
         val indentParent = 80
@@ -1051,30 +1057,14 @@ abstract class BaseEditorActivity :
     }
 
 
-    private fun appendClickableSpan(
-        sb: SpannableStringBuilder,
-        @StringRes textRes: Int,
-        span: ClickableSpan,
-    ) {
-        val str = getString(textRes)
-        val split = str.split("@@", limit = 3)
-        if (split.size != 3) {
-            // Not a valid format
-            sb.append(str)
-            sb.append('\n')
-            return
-        }
-        sb.append(split[0])
-        sb.append(split[1], span, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        sb.append(split[2])
-        sb.append('\n')
-    }
-
-    private fun setupBottomSheet() {
+	private fun setupBottomSheet() {
         editorBottomSheet = BottomSheetBehavior.from<View>(content.bottomSheet)
         BuildOutputProvider.setBottomSheet(content.bottomSheet)
         editorBottomSheet?.addBottomSheetCallback(object : BottomSheetCallback() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {
+				// update the sheet state so that the ViewModel is in sync
+				bottomSheetViewModel.setSheetState(sheetState = newState)
+
                 if (newState == BottomSheetBehavior.STATE_EXPANDED) {
                     val editor = provideCurrentEditor()
                     editor?.editor?.ensureWindowsDismissed()
@@ -1082,7 +1072,7 @@ abstract class BaseEditorActivity :
             }
 
             override fun onSlide(bottomSheet: View, slideOffset: Float) {
-                content.apply {
+				content.apply {
                     val editorScale = 1 - slideOffset * (1 - EDITOR_CONTAINER_SCALE_FACTOR)
                     this.bottomSheet.onSlide(slideOffset)
                     this.viewContainer.scaleX = editorScale
@@ -1132,15 +1122,7 @@ abstract class BaseEditorActivity :
         }
     }
 
-    private fun showNeedHelpDialog() {
-        val builder = newMaterialDialogBuilder(this)
-        builder.setTitle(string.need_help)
-        builder.setMessage(string.msg_need_help)
-        builder.setPositiveButton(android.R.string.ok, null)
-        builder.create().show()
-    }
-
-    open fun installationSessionCallback(): SessionCallback {
+	open fun installationSessionCallback(): SessionCallback {
         return ApkInstallationSessionCallback(this).also { installationCallback = it }
     }
 

app/src/main/java/com/itsaky/androidide/adapters/EditorBottomSheetTabAdapter.kt
@@ -50,7 +50,6 @@ class EditorBottomSheetTabAdapter(
 
 	private val allTabs =
 		mutableListOf<Tab>().apply {
-			@Suppress("KotlinConstantConditions")
 			add(
 				Tab(
 					title = fragmentActivity.getString(R.string.build_output),

app/src/main/java/com/itsaky/androidide/ui/EditorBottomSheet.kt
@@ -241,9 +241,7 @@ class EditorBottomSheet
 			binding.clearFab.setOnLongClickListener(generateTooltipListener(TooltipTag.OUTPUT_CLEAR))
 
 			binding.headerContainer.setOnClickListener {
-				if (behavior.state != BottomSheetBehavior.STATE_EXPANDED) {
-					behavior.state = BottomSheetBehavior.STATE_EXPANDED
-				}
+				viewModel.setSheetState(sheetState = BottomSheetBehavior.STATE_EXPANDED)
 			}
 
 			ViewCompat.setOnApplyWindowInsetsListener(this) { _, insets ->
@@ -347,7 +345,7 @@ class EditorBottomSheet
 
 			val padding = insetBottom * paddingScale
 			binding.headerContainer.apply {
-				updateLayoutParams<ViewGroup.LayoutParams> {
+				updateLayoutParams<LayoutParams> {
 					height = ((collapsedHeight + padding) * heightScale).roundToInt()
 				}
 				updatePaddingRelative(

app/src/main/java/com/itsaky/androidide/viewmodel/BottomSheetViewModel.kt
@@ -59,6 +59,7 @@ class BottomSheetViewModel : ViewModel() {
 		@TabDef currentTab: Int = this.sheetState.value.currentTab,
 	) {
 		val newState = SheetState(sheetState, currentTab)
+		logger.debug("new SheetState: {}", newState)
 		_sheetState.update { newState }
 	}
 }