65efded6bd8e330785f4c7f4254ef8156f8223d3
John Trujillo <johnandrestrujillo@gmail.com>
2025-10-02 18:08:11 -0500

    Unnecessary files removed

--- llama/build.gradle.kts
+++ llama/build.gradle.kts
@@ -10,8 +10,6 @@ android {
     defaultConfig {
         minSdk = 21
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
         ndk {
             // Add NDK properties if wanted, e.g.
             // abiFilters += listOf("arm64-v8a")
@@ -33,10 +31,6 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
     externalNativeBuild {
@@ -64,8 +58,4 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
