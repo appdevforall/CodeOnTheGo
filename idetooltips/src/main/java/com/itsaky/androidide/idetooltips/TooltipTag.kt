f030df89d783abeef8a54833f352c92bbe11c000
Oluwadara Abijo <oluwadara.abijo@appdevforall.org>
2025-10-07 18:47:24 +0100

    feat(ADFA-1668): Set tooltip on maximum line width dialog

--- idetooltips/src/main/java/com/itsaky/androidide/idetooltips/TooltipTag.kt
+++ idetooltips/src/main/java/com/itsaky/androidide/idetooltips/TooltipTag.kt
@@ -41,6 +41,7 @@ object TooltipTag {
     const val TEMPLATE_BASIC_ACTIVITY = "template.basic.activity"
     const val TEMPLATE_NO_ACTIVITY = "template.no.activity"
     const val TEMPLATE_NAV_DRAWER_ACTIVITY = "template.navdrawer.activity"
+    const val PREFS_EDITOR_XML = "prefs.editor.xml"
 
     // Editor screen
     const val EDITOR_PROJECT_OVERVIEW = "project.overview"
