01226fa5794ff962839434f068b77765768dc0e7
Akash Yadav <akashyadav@appdevforall.org>
2025-10-23 18:09:33 +0530

    fix: add test to ensure Lookup throws exception for re-registration
    Signed-off-by: Akash Yadav <akashyadav@appdevforall.org>

--- lookup/src/test/java/com/itsaky/androidide/lookup/internal/DefaultLookupTest.kt
+++ lookup/src/test/java/com/itsaky/androidide/lookup/internal/DefaultLookupTest.kt
@@ -3,6 +3,8 @@ package com.itsaky.androidide.lookup.internal
 import com.google.common.truth.Truth.assertThat
 import com.itsaky.androidide.lookup.LookupTest.TestService
 import com.itsaky.androidide.lookup.LookupTest.TestServiceImpl
+import com.itsaky.androidide.lookup.ServiceRegisteredException
+import org.junit.Assert.assertThrows
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -23,5 +25,18 @@ class DefaultLookupTest {
 		assertThat(lookup.keyTable[TestService::class.java]).isNotNull()
 
 		assertThat(lookup.lookup(TestService::class.java)).isEqualTo(service)
+	}
+
+	@Test
+	fun `test register throws exception when re-registering a service`() {
+		val lookup = DefaultLookup()
+		val service = TestServiceImpl()
+
+		lookup.register(TestService::class.java, service)
+		assertThat(lookup.lookup(TestService::class.java)).isNotNull()
+
+		assertThrows(ServiceRegisteredException::class.java) {
+			lookup.register(TestService::class.java, service)
+		}
 	}
 }
