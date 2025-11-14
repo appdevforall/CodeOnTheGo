ca2e00f715ad9bcb33edaebb195412293518119c
Oluwadara Abijo <oluwadara.abijo@appdevforall.org>
2025-11-14 15:45:47 +0100

    docs(ADFA-2024): Add necessary comment

--- app/src/main/java/com/itsaky/androidide/utils/StorageSizeUtils.kt
+++ app/src/main/java/com/itsaky/androidide/utils/StorageSizeUtils.kt
@@ -1,5 +1,10 @@
 package com.itsaky.androidide.utils
 
+/**
+ * Using binary prefix (1024^3) for Gibibytes (GiB) to be consistent with how Android's file
+ * system APIs report storage space. While storage manufacturers use decimal
+ * Gigabytes (GB, 1000^3), OSes use binary.
+ * */
 private const val BYTES_IN_GIGABYTE = 1024F * 1024F * 1024F
 
 /** Converts a Long representing bytes into Gigabytes. */
