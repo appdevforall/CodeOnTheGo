e17d6173adb2da69d1153686eaeec9f001a376c0
John Andrés Trujillo <34223334+jatezzz@users.noreply.github.com>
2025-11-21 08:16:40 -0500

    ADFA-1888 | Refactor: Modernize build.gradle template syntax (#638)
    * refactor(templates-api): modernize build.gradle template syntax
    Update Groovy DSL to use assignment (=) and remove duplicate exclusion
    
    * refactor(syntax): Missing space in method name androidPluginGroovy()

--- templates-impl/src/main/java/com/itsaky/androidide/templates/impl/composeActivity/composeActivityTemplate.kt
+++ templates-impl/src/main/java/com/itsaky/androidide/templates/impl/composeActivity/composeActivityTemplate.kt
@@ -57,7 +57,7 @@ fun composeActivityProject() =
 
 				require(data.language == Kotlin) { "Compose activity requires Kotlin language" }
 
-				composeDependencies()
+				composeDependencies(data.useKts)
 
 				res {
 					writeXmlResource("themes", VALUES, source = ::composeThemesXml)
