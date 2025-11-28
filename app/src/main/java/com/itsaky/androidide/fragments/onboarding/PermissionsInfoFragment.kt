c4b4b3e3acb40a1963aa548df49d734b4af0e5be
Oluwadara Abijo <oluwadara.abijo@appdevforall.org>
2025-11-28 16:19:50 +0100

    refactor(ADFA-2195): Remove unused import

--- app/src/main/java/com/itsaky/androidide/fragments/onboarding/PermissionsInfoFragment.kt
+++ app/src/main/java/com/itsaky/androidide/fragments/onboarding/PermissionsInfoFragment.kt
@@ -1,7 +1,6 @@
 package com.itsaky.androidide.fragments.onboarding
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
