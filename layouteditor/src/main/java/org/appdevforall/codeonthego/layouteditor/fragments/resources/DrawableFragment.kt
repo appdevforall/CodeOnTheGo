807b1c7032fb7c1a18655bd50f34fa962ad643d4
Oluwadara Abijo <oluwadara.abijo@appdevforall.org>
2025-12-11 14:04:59 +0100

    refactor(ADFA-2310): Move image decoding to background thread

--- layouteditor/src/main/java/org/appdevforall/codeonthego/layouteditor/fragments/resources/DrawableFragment.kt
+++ layouteditor/src/main/java/org/appdevforall/codeonthego/layouteditor/fragments/resources/DrawableFragment.kt
@@ -1,13 +1,11 @@
 package org.appdevforall.codeonthego.layouteditor.fragments.resources
 
 import android.content.Context
-import android.content.DialogInterface
 import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
-import android.text.TextUtils
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
@@ -15,11 +13,15 @@ import android.view.ViewGroup
 import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.blankj.utilcode.util.ToastUtils
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.apache.commons.io.FileUtils
 import org.appdevforall.codeonthego.layouteditor.ProjectFile
 import org.appdevforall.codeonthego.layouteditor.R
@@ -37,8 +39,6 @@ import org.appdevforall.codeonthego.layouteditor.utils.Utils
 import org.slf4j.LoggerFactory
 import java.io.File
 import java.io.IOException
-import java.util.concurrent.ExecutionException
-import java.util.concurrent.Executors
 
 class DrawableFragment : Fragment {
     private var binding: FragmentResourcesBinding? = null
@@ -85,158 +85,171 @@ class DrawableFragment : Fragment {
     }
 
     fun loadDrawables() {
-        val executorService = Executors.newSingleThreadExecutor()
-        val future =
-            executorService.submit<Void?> {
-                var version = 0
-                val drawableFolder = File(project!!.path + "/drawable/")
-                if (drawableFolder.exists()) {
-                    val drawables =
-                        FileUtils.listFiles(
-                            drawableFolder,
+        val appContext = requireContext().applicationContext
+
+        lifecycleScope.launch {
+
+            try {
+                val result = withContext(Dispatchers.IO) {
+
+                    val list = mutableListOf<DrawableFile>()
+                    val baseDrawableFolder = File(project!!.path + "/drawable/")
+
+                    if (baseDrawableFolder.exists()) {
+                        val drawables = FileUtils.listFiles(
+                            baseDrawableFolder,
                             arrayOf("png", "jpg", "jpeg", "gif", "xml"),
                             false
                         )
-                    for (drawable in drawables) {
-                        val drawableName = drawable.name
-                        val drawableObj =
-                            if (drawableName.endsWith(".xml"))
+
+                        for (file in drawables) {
+                            var version = 0
+                            val name = file.name
+
+                            val drawable = if (name.endsWith(".xml")) {
                                 Utils.getVectorDrawableAsync(
-                                    requireContext(),
-                                    Uri.fromFile(drawable)
+                                    appContext,          // <-- FIX
+                                    Uri.fromFile(file)
                                 )
-                            else
-                                Drawable.createFromPath(drawable.path)
-                        for (i in dpiList!!.indices) {
-                            val dpiFolder =
-                                File(project!!.path + "/drawable-" + dpiList!![i] + "/")
-                            if (dpiFolder.exists()) {
-                                val matchingFile = File(dpiFolder, drawableName)
-                                if (matchingFile.exists()) {
-                                    Drawable.createFromPath(matchingFile.path)
-                                    version = i
+                            } else {
+                                Drawable.createFromPath(file.path)
+                            }
+
+                            dpiList?.forEachIndexed { i, dpi ->
+                                val dpiFolder = File(project!!.path + "/drawable-$dpi/")
+                                if (dpiFolder.exists()) {
+                                    val matching = File(dpiFolder, name)
+                                    if (matching.exists()) {
+                                        Drawable.createFromPath(matching.path)
+                                        version = i
+                                    }
                                 }
                             }
-                        }
-                        drawableList.add(
-                            DrawableFile(
-                                version + 1,
-                                drawableObj!!,
-                                drawable.path
-                            )
-                        )
-                    }
-                }
-                null
-            }
 
-        try {
-            future.get()
-        } catch (e: ExecutionException) {
-            logger.error("Error loading drawable", e)
-        } catch (e: InterruptedException) {
-            logger.error("Interruption while loading drawable", e)
-        } finally {
-            executorService.shutdown()
+                            drawable?.let {
+                                list.add(
+                                    DrawableFile(
+                                        version + 1,
+                                        it,
+                                        file.path
+                                    )
+                                )
+                            }
+                        }
+                    }
+
+                    list
+                }
+
+                drawableList.clear()
+                drawableList.addAll(result)
+                adapter?.notifyDataSetChanged()
+
+            } catch (e: Exception) {
+                logger.error("Error loading drawables", e)
+            }
         }
     }
 
     fun addDrawable(uri: Uri) {
         val path = FileUtil.convertUriToFilePath(requireContext(), uri)
-        if (TextUtils.isEmpty(path)) {
+        if (path.isEmpty()) {
             ToastUtils.showLong(R.string.invalid_data_intent)
             return
         }
-        // File name with extension
+
         val lastSegment = getLastSegmentFromPath(path)
-
-        // File name without extension
         val fileName = lastSegment.substring(0, lastSegment.lastIndexOf("."))
+        val extension = lastSegment.substring(lastSegment.lastIndexOf("."))
 
-        // Extension
-        val extension =
-            lastSegment.substring(lastSegment.lastIndexOf("."), lastSegment.length)
         val builder = MaterialAlertDialogBuilder(requireContext())
-        val dialogBinding =
-            DialogSelectDpisBinding.inflate(builder.create().layoutInflater)
+        val dialogBinding = DialogSelectDpisBinding.inflate(builder.create().layoutInflater)
         val editText = dialogBinding.textinputEdittext
         val inputLayout = dialogBinding.textinputLayout
+
         inputLayout.setHint(R.string.msg_enter_new_name)
         editText.setText(fileName)
 
         if (!lastSegment.endsWith(".xml")) {
-            dpiAdapter = DPIsListAdapter(Drawable.createFromPath(path)!!)
-            dialogBinding.listDpi.setAdapter(dpiAdapter)
-            dialogBinding.listDpi.setLayoutManager(GridLayoutManager(requireActivity(), 2))
+            Drawable.createFromPath(path)?.let { drawable ->
+                dpiAdapter = DPIsListAdapter(drawable)
+            }
+            dialogBinding.listDpi.adapter = dpiAdapter
+            dialogBinding.listDpi.layoutManager = GridLayoutManager(requireActivity(), 2)
         }
+
         dialogBinding.listDpi.visibility =
             if (lastSegment.endsWith(".xml")) View.GONE else View.VISIBLE
 
-        builder.setView(dialogBinding.getRoot())
+        builder.setView(dialogBinding.root)
         builder.setTitle(R.string.add_drawable)
-        builder.setNegativeButton(
-            R.string.cancel
-        ) { di: DialogInterface?, which: Int -> }
-        builder.setPositiveButton(
-            R.string.add
-        ) { di: DialogInterface?, which: Int ->
-            val drawablePath = project!!.drawablePath
-            var version = 0
-            if (!lastSegment.endsWith(".xml") && dpiAdapter != null) {
-                val selectedDPIs = dpiAdapter!!.selectedItems
-                for (i in selectedDPIs.indices) {
-                    try {
-                        ImageConverter.convertToDrawableDpis(
-                            editText.getText().toString() + extension,
-                            BitmapFactory.decodeFile(path),
-                            selectedDPIs
-                        )
-                    } catch (e: IOException) {
-                        e.printStackTrace()
-                    }
-                    version = i
-                }
-            }
-            val toPath = drawablePath + editText.getText().toString() + extension
-            FileUtil.copyFile(uri, toPath, requireContext())
+        builder.setNegativeButton(R.string.cancel, null)
 
-            val drawable =
-                if (lastSegment.endsWith(".xml"))
-                    Utils.getVectorDrawableAsync(requireContext(), Uri.fromFile(File(toPath)))
-                else
-                    Drawable.createFromPath(toPath)
-            val drawableFile = DrawableFile(version + 1, drawable!!, toPath)
-            drawableList.add(drawableFile)
-            adapter!!.notifyDataSetChanged()
+        builder.setPositiveButton(R.string.add) { _, _ ->
+            lifecycleScope.launch {
+                val newDrawableFile = withContext(Dispatchers.IO) {
+                    val drawablePath = project!!.drawablePath
+                    var version = 0
+
+                    if (!lastSegment.endsWith(".xml") && dpiAdapter != null) {
+                        val selectedDPIs = dpiAdapter!!.selectedItems
+
+                        selectedDPIs.forEachIndexed { i, dpi ->
+                            try {
+                                ImageConverter.convertToDrawableDpis(
+                                    editText.text.toString() + extension,
+                                    BitmapFactory.decodeFile(path),
+                                    selectedDPIs
+                                )
+                            } catch (_: IOException) { }
+                            version = i
+                        }
+                    }
+
+                    val toPath = drawablePath + editText.text.toString() + extension
+                    FileUtil.copyFile(uri, toPath, requireContext())
+
+                    val drawable =
+                        if (lastSegment.endsWith(".xml"))
+                            Utils.getVectorDrawableAsync(requireContext(), Uri.fromFile(File(toPath)))
+                        else
+                            Drawable.createFromPath(toPath)
+
+                    DrawableFile(version + 1, drawable!!, toPath)
+                }
+
+                drawableList.add(newDrawableFile)
+                adapter?.notifyDataSetChanged()
+            }
         }
 
         val dialog = builder.create()
         dialog.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
         dialog.show()
 
-        editText.addTextChangedListener(
-            object : TextWatcher {
-                override fun beforeTextChanged(p1: CharSequence?, p2: Int, p3: Int, p4: Int) {}
-
-                override fun onTextChanged(p1: CharSequence?, p2: Int, p3: Int, p4: Int) {}
-
-                override fun afterTextChanged(p1: Editable?) {
-                    NameErrorChecker.checkForDrawable(
-                        editText.getText().toString(), inputLayout, dialog, drawableList
-                    )
-                }
-            })
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                NameErrorChecker.checkForDrawable(
+                    editText.text.toString(),
+                    inputLayout,
+                    dialog,
+                    drawableList
+                )
+            }
+        })
 
         NameErrorChecker.checkForDrawable(fileName, inputLayout, dialog, drawableList)
 
         editText.requestFocus()
-        val inputMethodManager =
-            dialogBinding.getRoot().context
-                .getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodManager.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
+        val imm = dialogBinding.root.context
+            .getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
 
-        if (editText.getText().toString() != "") {
-            editText.setSelection(0, editText.getText().toString().length)
+        if (editText.text?.isNotEmpty() == true) {
+            editText.setSelection(0, editText.text!!.length)
         }
     }
+
 }
