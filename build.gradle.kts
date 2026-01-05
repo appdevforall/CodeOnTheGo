80c4041d109837ac9a5faada5802482389b4c5df
Joel Menchavez <joelmenchavez@appdevforall.org>
2026-01-05 14:52:44 +0800

    add in code taken out during rebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,6 +67,14 @@ subprojects {
         toolVersion = "0.8.11"
     }
 
+    plugins.withId("com.android.library") {
+        sonar {
+            properties {
+                property("sonar.androidVariant", "v8Debug")
+            }
+        }
+    }
+
     // Always load the F-Droid config
 	FDroidConfig.load(project)
 
@@ -295,7 +303,7 @@ sonar {
     }
 }
 
-tasks.named("sonar") {
+tasks.named("sonarqube") {
     dependsOn("jacocoAggregateReport")
 }
 
