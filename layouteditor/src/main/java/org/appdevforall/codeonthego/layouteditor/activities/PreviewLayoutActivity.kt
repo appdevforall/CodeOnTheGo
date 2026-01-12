7504d121379cfd9d943eb0227c06c9100a993573
John Trujillo <johnandrestrujillo@gmail.com>
2026-01-12 16:28:59 -0500

    fix: layouteditor display error dialog when layout parsing fails

--- layouteditor/src/main/java/org/appdevforall/codeonthego/layouteditor/activities/PreviewLayoutActivity.kt
+++ layouteditor/src/main/java/org/appdevforall/codeonthego/layouteditor/activities/PreviewLayoutActivity.kt
@@ -3,9 +3,11 @@ package org.appdevforall.codeonthego.layouteditor.activities
 import android.os.Bundle
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams
+import androidx.appcompat.app.AlertDialog
 import org.appdevforall.codeonthego.layouteditor.BaseActivity
 import org.appdevforall.codeonthego.layouteditor.LayoutFile
 import org.appdevforall.codeonthego.layouteditor.R
+import com.itsaky.androidide.resources.R.string
 import org.appdevforall.codeonthego.layouteditor.databinding.ActivityPreviewLayoutBinding
 import org.appdevforall.codeonthego.layouteditor.tools.XmlLayoutParser
 import org.appdevforall.codeonthego.layouteditor.utils.Constants
@@ -15,8 +17,8 @@ class PreviewLayoutActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityPreviewLayoutBinding.inflate(layoutInflater)
         setContentView(binding.getRoot())
-        @Suppress("DEPRECATION") val layoutFile =
-            intent.extras!!.getParcelable<LayoutFile>(Constants.EXTRA_KEY_LAYOUT)
+        @Suppress("DEPRECATION")
+        val layoutFile = intent.extras?.getParcelable<LayoutFile>(Constants.EXTRA_KEY_LAYOUT)
         val parser = XmlLayoutParser(this)
         layoutFile?.readDesignFile()?.let { parser.parseFromXml(it, this) }
 
@@ -27,6 +29,23 @@ class PreviewLayoutActivity : BaseActivity() {
             LayoutParams.MATCH_PARENT
         )
 
-        previewContainer.addView(parser.root, layoutParams)
+        parser.root?.let { rootView ->
+            (rootView.parent as? ViewGroup)?.removeView(rootView)
+            previewContainer.addView(rootView, layoutParams)
+        } ?: run {
+            showErrorDialog()
+        }
+    }
+
+    private fun showErrorDialog() {
+        AlertDialog.Builder(this)
+            .setTitle(applicationContext.getString(string.preview_render_error_title))
+            .setMessage(applicationContext.getString(string.preview_render_error_message))
+            .setPositiveButton(applicationContext.getString((string.msg_ok))) { dialog, _ ->
+                dialog.dismiss()
+                finish()
+            }
+            .setCancelable(false)
+            .show()
     }
 }
