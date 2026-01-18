0a6261beac261bc9aef7c47f6dc3cfa978f729a3
Daniel-ADFA <danielalome@appdevforall.org>
2026-01-18 21:19:14 +0100

    ADFA-2358:  fix IllegalArgumentException: Invalid path: /data/data/com.itsaky.androidide/files (#785)
    * Rename .java to .kt
    
    * Fix EACCES permission errors during Direct Boot mode
    
    * fixes
    
    * Address PR review comments for Environment initialization
    
    ---------
    
    Co-authored-by: ADFA <dev-team@appdevforall.org>

--- app/src/main/java/com/itsaky/androidide/app/DeviceProtectedApplicationLoader.kt
+++ app/src/main/java/com/itsaky/androidide/app/DeviceProtectedApplicationLoader.kt
@@ -18,6 +18,7 @@ import com.itsaky.androidide.events.ProjectsApiEventsIndex
 import com.itsaky.androidide.handlers.CrashEventSubscriber
 import com.itsaky.androidide.syntax.colorschemes.SchemeAndroidIDE
 import com.itsaky.androidide.ui.themes.IThemeManager
+import com.itsaky.androidide.utils.Environment
 import com.itsaky.androidide.utils.FeatureFlags
 import com.termux.shared.reflection.ReflectionUtils
 import io.github.rosemoe.sora.widget.schemes.EditorColorScheme
@@ -50,6 +51,10 @@ internal object DeviceProtectedApplicationLoader :
 
 	override suspend fun load(app: IDEApplication) {
 		logger.info("Loading device protected storage context components...")
+
+		runCatching {
+			Environment.init(app)
+		}
 
 		runCatching {
 			// try to initialize feature flags
