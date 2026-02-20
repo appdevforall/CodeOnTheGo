84f5d1ee47896e8c93eabd6b6f5c148522575b08
itsaky-adfa <akashyadav@appdevforall.org>
2026-02-20 18:45:49 +0530

    ADFA-2784: add Gradle build tuner (#973)
    * feat: add Gradle build tuner
    * feat: add helpers to build device profile
    * feat: allow tooling clients to specify global Gradle build parameters
    * feat: add logic to compute CPU topology and thermal state
    
    For CPU topology :
      1. Try to determine clusters using cpufreq policy directories:
         this is probably the most reliable option to determine the CPU topology
         and maps 1:1 on to a core cluster on heterogeneous SoCs.
      2. Read max frequency of each CPU: group CPUs based on their
         maximum frequency, sort them in ascending order. CPUs with
         minimum frequency become the little CPUs, and the same
         with max frequency become the prime CPUs, while the rest
         become big CPUs.
      3. Fall back to symmetric grouping. The number of available CPUs
         become big CPUs, while little and prime CPUs are 0.
    
    For thermal state:
      1. On Android 29+: use PowerManager.getCurrentThermalStatus()
      2. On Android 28: read /sys/class/thermal/thermal_zone*/{temp, trip_point_N_temp}
         and compare `temp` with `trip_point_N_temp` to determine whether the given
         thermal zone is thermal-throttled or not.
    
    * feat: integrate Gradle build tuning strategies as an experimental feature
    * tests: add test to verify generate Gradle args correctness
    * fix: previous tunig config is not provided when new config is built
    * fix: use serial GC on low-memory devices
    * fix: track metrics related to performance tuning
    * refactor: reformat
    * fix: kt max heap is doubled instead of halved in balanced strategy
    * fix: skip clusters whose cpuinfo_max_freq could not be computed
    * fix: remove hardcoded isDebugBuild=false
    * fix: use kotlin.daemon.jvmargs for some Gradle versions
    * fix: use mem-bound Gradle daemon xmx limit on very low-memory devices
    * tests: fix compilation error
    * fix: inconsistent build duration computation during initialization
    * fix: gracefully handle failures during prepareBuild
    * fix: resolve compilation errors in MultiVersionTestClient
    * refactor: reformat
    * fix: make strategies objects instead of classes
    * fix: gracefully handle failures during Gradle auto-tune
    * fix: use actual CPU count when determining clusters using policies
    * fix: restore interrupted status when preparing build in tooling server
    * refactor: reformat
    * fix: InitializeProjectParams is not serializable
    * fix: do not set android.enableR8
    
    The property is deprecated and removed in newer versions of AGP
    
    * fix: report build auto-tune exceptions to Sentry
    * docs: update docs for GradleTuningConfig
    * fix: ensure kotlin Xmx value is capped at gradleXmx/2
    * refactor: reformat
    * fix: make Xmx values fully memory-bound
    * fix: minor improvements
    
    Signed-off-by: Akash Yadav <akashyadav@appdevforall.org>

---
+++ subprojects/tooling-api/src/main/java/com/itsaky/androidide/tooling/api/messages/BuildId.kt
@@ -0,0 +1,17 @@
+package com.itsaky.androidide.tooling.api.messages
+
+import java.io.Serializable
+
+/**
+ * A build identifier.
+ *
+ * @author Akash Yadav
+ */
+data class BuildId(
+	val buildSessionId: String,
+	val buildId: Long,
+) : Serializable {
+	companion object {
+		val Unknown = BuildId("unknown", -1)
+	}
+}
