feb3153d3fae1ff0c0aa0a1663ff8cb4a9f12c93
davidschachterADFA <davidschachter@appdevforall.org>
2026-02-26 13:03:44 -0800

    fix/ADFA-680-685, 2037, 1103 Fix improper use of COGO, etc. in user-visible strings (#1020)
    * fix/ADFA-680-685, 2037, 1103 Fix improper use of COGO and variants in user-visible strings
    
    * fix/ADFA-680-685, 2037, 1103 address CodeRabbit suggestions, fixed use of 'CodeOnTheGo'.
    
    * fix/ADFA-685 Remove a stray word.
    
    * fix/ADFA-685 Revert a change suggested by AI that triggered a cascade of errors.
    
    ---------
    
    Co-authored-by: Hal Eisen <haleisen@appdevforall.org>

--- common/src/main/java/com/itsaky/androidide/activities/editor/HelpActivity.kt
+++ common/src/main/java/com/itsaky/androidide/activities/editor/HelpActivity.kt
@@ -1,5 +1,5 @@
 /*
- *  This file is part of CodeOnTheGo.
+ *  This file is part of Code on the Go.
  *
  *  AndroidIDE is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
