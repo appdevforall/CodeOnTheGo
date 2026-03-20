c9212217b0a9e62a2d003167449d05bac06c0650
itsaky-adfa <akashyadav@appdevforall.org>
2026-03-20 15:51:24 +0530

    ADFA-2682: use random port to listen for JDWP connections (#1080)
    * fix: use random port to listen for JDWP connections
    * fix: avoid inconsistent listenAddress in ListenerState
    * fix: add retry mechanism for fetching active JDWP port
    
    Signed-off-by: Akash Yadav <akashyadav@appdevforall.org>

--- lsp/java/src/main/java/com/itsaky/androidide/lsp/java/debug/ListenerState.kt
+++ lsp/java/src/main/java/com/itsaky/androidide/lsp/java/debug/ListenerState.kt
@@ -3,33 +3,56 @@ package com.itsaky.androidide.lsp.java.debug
 import com.itsaky.androidide.lsp.debug.IDebugClient
 import com.sun.jdi.VirtualMachine
 import com.sun.jdi.connect.Connector
+import com.sun.jdi.connect.IllegalConnectorArgumentsException
 import com.sun.tools.jdi.SocketListeningConnector
 import com.sun.tools.jdi.isListening
+import java.io.IOException
 
 internal data class ListenerState(
-	val client: IDebugClient,
-	val connector: SocketListeningConnector,
-	val args: Map<String, Connector.Argument>,
+    val client: IDebugClient,
+    val connector: SocketListeningConnector,
+    val args: Map<String, Connector.Argument>
 ) {
+
+	/**
+	 * Whether we're currently listening for incoming connections.
+	 */
 	val isListening: Boolean
 		get() = connector.isListening(args)
 
 	/**
-	 * Start listening for connections from VMs.
-	 *
-	 * @return The address of the listening socket.
+	 * The address we're listening at. May be `null` when we're not listening.
 	 */
-	fun startListening(): String = connector.startListening(args)
+	var listenAddress: String? = null
+		private set
+		get() = field.takeIf { isListening }
 
-	/**
-	 * Stop listening for connections from VMs.
-	 */
-	fun stopListening() = connector.stopListening(args)
+    /**
+     * Start listening for connections from VMs.
+     *
+     * @return The address of the listening socket.
+     */
+    fun startListening(): String {
+		val address = connector.startListening(args)
+		listenAddress = address
+		return address
+	}
 
-	/**
-	 * Accept a connection from a VM.
-	 *
-	 * @return The connected VM.
-	 */
-	fun accept(): VirtualMachine = connector.accept(args)
+    /**
+     * Stop listening for connections from VMs.
+     */
+    fun stopListening() {
+		try {
+			connector.stopListening(args)
+		} finally {
+			listenAddress = null
+		}
+	}
+
+    /**
+     * Accept a connection from a VM.
+     *
+     * @return The connected VM.
+     */
+    fun accept(): VirtualMachine = connector.accept(args)
 }
