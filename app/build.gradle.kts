87e7a390dcfcba51d570b9aaad1e4b3248b14361
jomen-adfa <joelmenchavez@appdevforall.org>
2026-03-24 12:43:39 +0900

    next gen template initial version (#1069)
    * next gen template initial version
    
    * indent issue
    
    * update minsdk reference
    
    * template archive installation
    
    * cleanup of widget references and zip issues
    
    * cleanup imports
    
    * take out null checks
    
    * restore leak canary
    
    * set core.cgt.br expected size
    
    * prevent zip slip
    
    * take out unused import
    
    * initial add widgets
    
    * user defined parameters
    
    * unused imports
    
    * modify log entry
    
    * exclude basepath i.e. template root folder in generated project
    
    * read tooltipTag from template.json
    
    * uncomment lib entry for fragment-ktx and take out comments
    
    * use File.separator instead of declaring a constant ZIP_SEPARATOR and remove extraneous comments
    
    * readable warnings log
    
    * proguard rules to prevent obfuscation of gson used classes
    
    * clean out extraneous debugging lines
    
    * remove unnecessary log object
    
    * formatting
    
    * formatting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -541,6 +541,7 @@ fun createAssetsZip(arch: String) {
 			"documentation.db",
 			bootstrapName,
 			"plugin-artifacts.zip",
+            "core.cgt"
 		).forEach { fileName ->
 			val filePath = sourceDir.resolve(fileName)
 			if (!filePath.exists()) {
@@ -1059,6 +1060,12 @@ val debugAssets =
 			"localMvnRepository.zip",
 			"debug",
 		),
+        Asset(
+          "assets/core.cgt",
+          "https://appdevforall.org/dev-assets/debug/core.cgt",
+          "core.cgt",
+          "debug",
+        ),
 	)
 
 val releaseAssets =
@@ -1111,6 +1118,12 @@ val releaseAssets =
 			"v8/bootstrap.zip.br",
 			"release",
 		),
+        Asset(
+          "assets/release/common/data/common/core.cgt.br",
+          "https://appdevforall.org/dev-assets/release/core.cgt.br",
+          "core.cgt.br",
+          "release",
+        ),
 	)
 
 fun assetsBatch(
---
+++ templates-impl/src/main/java/com/itsaky/androidide/templates/impl/zip/ZipTemplateReader.kt
@@ -0,0 +1,123 @@
+package com.itsaky.androidide.templates.impl.zip
+
+import com.google.gson.Gson
+import com.itsaky.androidide.templates.CheckBoxWidget
+import com.itsaky.androidide.templates.ModuleTemplateData
+import com.itsaky.androidide.templates.Parameter
+import com.itsaky.androidide.templates.ProjectTemplate
+import com.itsaky.androidide.templates.ProjectTemplateData
+import com.itsaky.androidide.templates.ProjectTemplateRecipeResult
+import com.itsaky.androidide.templates.R
+import com.itsaky.androidide.templates.TemplateRecipe
+import com.itsaky.androidide.templates.TextFieldWidget
+import com.itsaky.androidide.templates.Widget
+import com.itsaky.androidide.templates.base.baseZipProject
+import com.itsaky.androidide.templates.booleanParameter
+import com.itsaky.androidide.templates.stringParameter
+import org.slf4j.LoggerFactory
+import java.io.File
+import java.util.zip.ZipFile
+
+object ZipTemplateReader {
+    private val log = LoggerFactory.getLogger(ZipTemplateReader::class.java)
+
+    private val gson = Gson()
+
+    fun read(
+        zipFile: File,
+        recipeFactory: (TemplateJson, MutableMap<String, Parameter<*>>, String, ProjectTemplateData, ModuleTemplateData) -> TemplateRecipe<ProjectTemplateRecipeResult>
+    ): List<ProjectTemplate> {
+
+        val templates = mutableListOf<ProjectTemplate>()
+
+        try {
+            ZipFile(zipFile).use { zip ->
+
+                log.debug("zipFile: $zipFile")
+
+                val indexEntry = zip.getEntry(ARCHIVE_JSON) ?: return emptyList()
+                val indexJson = zip.getInputStream(indexEntry).bufferedReader().use {
+                    gson.fromJson(it, TemplatesIndex::class.java)
+                }
+
+                log.debug("indexJson: $indexJson")
+                for (templateRef in indexJson.templates) {
+                    try {
+                        val basePath = templateRef.path
+                        log.debug("basePath: $basePath")
+                        val metaEntry = zip.getEntry("$basePath/$META_FOLDER/$META_JSON") ?: continue
+
+                        val metaJsonString = zip.getInputStream(metaEntry).bufferedReader().use { reader ->
+                            reader.readText()
+                        }
+
+                        val metaJson = gson.fromJson(metaJsonString, TemplateJson::class.java)
+
+                        log.debug("metaJson: $metaJson")
+
+                        val thumbEntry = zip.getEntry("$basePath/$META_FOLDER/$META_THUMBNAIL")
+                        val thumbData = thumbEntry?.let { zip.getInputStream(it).use { s -> s.readBytes() } }
+
+                        if (thumbData == null) log.error("template $basePath/$META_FOLDER/$META_THUMBNAIL not found or is invalid")
+                        log.debug("thumbData: $thumbData")
+
+                        val userWidgets = mutableListOf<Widget<*>>()
+                        val params = mutableMapOf<String, Parameter<*>>()
+
+                        metaJson.parameters?.user?.text?.forEach { textParam ->
+                            val param = stringParameter {
+                                name = 0
+                                nameStr = textParam.label ?: ""
+                                default = textParam.default ?: ""
+                            }
+                            userWidgets.add(TextFieldWidget(param))
+                            params[textParam.identifier] = param
+                        }
+
+                        metaJson.parameters?.user?.checkbox?.forEach { checkboxParam ->
+                            val param = booleanParameter {
+                                name = 0
+                                nameStr = checkboxParam.label ?: ""
+                                default = checkboxParam.default ?: false
+                            }
+                            userWidgets.add(CheckBoxWidget(param))
+                            params[checkboxParam.identifier] = param
+                        }
+
+                        val project = baseZipProject(
+                            showLanguage = (metaJson.parameters?.optional?.language != null),
+                            showMinSdk = (metaJson.parameters?.optional?.minsdk != null)
+                        ) {
+
+                            this.templateNameStr = metaJson.name
+                            this.tooltipTag = metaJson.tooltipTag
+                            this.thumbData = thumbData
+
+                            this.templateName = 0
+                            this.thumb = R.drawable.template_no_activity
+
+                            for (widget in userWidgets) {
+                                widgets(widget)
+                            }
+
+                            log.debug("this.name: ${this.templateNameStr}")
+                            this.recipe = TemplateRecipe { executor ->
+                                val innerRecipe = recipeFactory(metaJson, params, basePath, data, defModule)
+                                innerRecipe.execute(executor)
+                            }
+                        }
+
+                        log.debug("adding project ${metaJson.name}")
+                        templates.add(project)
+                    } catch (e: Exception) {
+                        log.error("Failed to load template at ${templateRef.path}", e)
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            log.error("Failed to read zip file $zipFile", e)
+        }
+
+        return templates
+    }
+}
