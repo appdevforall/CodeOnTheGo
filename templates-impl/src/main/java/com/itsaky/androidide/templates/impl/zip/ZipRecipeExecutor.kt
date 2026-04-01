69601a085b9b45fae8f3105b0a5f4ae1af712d84
Daniel Alome <danielalome@appdevforall.org>
2026-04-01 12:25:43 +0100

    Add CLASS_NAME support and fix Pebble template rendering for plugin templates
      - Add CLASS_NAME token replacement in ZipRecipeExecutor for dynamic class
        naming in file paths and template content
      - Add KEY_CLASS_NAME constant to ZipTemplateConstants
      - Add defaultAppName field to TemplateJson for per-template project name defaults

--- templates-impl/src/main/java/com/itsaky/androidide/templates/impl/zip/ZipRecipeExecutor.kt
+++ templates-impl/src/main/java/com/itsaky/androidide/templates/impl/zip/ZipRecipeExecutor.kt
@@ -34,6 +34,7 @@ class ZipRecipeExecutor(
 
     companion object {
         private val log = LoggerFactory.getLogger(ZipRecipeExecutor::class.java)
+        private val CLASS_NAME_PATTERN = Regex("[^a-zA-Z0-9]")
     }
 
     override fun execute(
@@ -70,7 +71,9 @@ class ZipRecipeExecutor(
             .syntax(customSyntax)
             .build()
 
-        val (identifiers, warnings) = metaJson.pebbleParams(data, defModule, params)
+        val className = data.name.replace(CLASS_NAME_PATTERN, "")
+        val (baseIdentifiers, warnings) = metaJson.pebbleParams(data, defModule, params)
+        val identifiers = baseIdentifiers + (KEY_CLASS_NAME to className)
         log.debug("identifiers warnings: ${warnings.joinToString(System.lineSeparator())}")
 
         val packageName =
@@ -93,6 +96,7 @@ class ZipRecipeExecutor(
 
             val relativePath = normalized.removePrefix("$basePath/")
                 .replace(packageName.value, defModule.packageName.replace(".", "/"))
+                .replace(KEY_CLASS_NAME, className)
 
             val outFile = File(projectDir, relativePath.removeSuffix(TEMPLATE_EXTENSION)).canonicalFile
 
