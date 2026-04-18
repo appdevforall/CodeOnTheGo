98b7dc3dcc7a037c99954bdfdbf8865eb77c1e99
Akash Yadav <akashyadav@appdevforall.org>
2026-04-18 19:03:00 +0530

    fix: add cancellation support for Kotlin code completions
    Signed-off-by: Akash Yadav <akashyadav@appdevforall.org>

--- lsp/kotlin/src/main/java/com/itsaky/androidide/lsp/kotlin/completion/KotlinSnippetRepository.kt
+++ lsp/kotlin/src/main/java/com/itsaky/androidide/lsp/kotlin/completion/KotlinSnippetRepository.kt
@@ -1,13 +1,9 @@
 package com.itsaky.androidide.lsp.kotlin.completion
 
-import com.itsaky.androidide.lsp.snippets.ISnippet
 import com.itsaky.androidide.lsp.snippets.SnippetParser
 
 object KotlinSnippetRepository {
-	lateinit var snippets: Map<KotlinSnippetScope, List<ISnippet>>
-		private set
-
-	fun init() {
-		snippets = SnippetParser.parse("kt", KotlinSnippetScope.entries)
+	val snippets by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
+		SnippetParser.parse("kt", KotlinSnippetScope.entries)
 	}
 }
