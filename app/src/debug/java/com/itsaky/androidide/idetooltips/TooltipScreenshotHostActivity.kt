ad4317c95bf5e862694fb1f3eb987c105518b85b
Hal Eisen <haleisen@appdevforall.org>
2026-05-13 16:24:56 -0700

    Delete app/src/debug/java/com/itsaky/androidide/idetooltips/TooltipScreenshotHostActivity.kt

--- app/src/debug/java/com/itsaky/androidide/idetooltips/TooltipScreenshotHostActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.itsaky.androidide.idetooltips
-
-import androidx.appcompat.app.AppCompatActivity
-
-/**
- * Empty host activity used by TooltipDebugDialogScreenshotTest to anchor
- * a tooltip popup. Debug-only because it has no production purpose.
- */
-class TooltipScreenshotHostActivity : AppCompatActivity()
