57736c39c53778e68ffb6b65c039470ccb1b6a81
Oluwadara Abijo <oluwadara.abijo@appdevforall.org>
2026-08-04 17:28:58 +0100

    feat(ADFA-934): Persist output view options in preferences

--- preferences/src/main/java/com/itsaky/androidide/preferences/internal/EditorPreferences.kt
+++ preferences/src/main/java/com/itsaky/androidide/preferences/internal/EditorPreferences.kt
@@ -38,6 +38,9 @@ object EditorPreferences {
 	const val FLAG_PASSWORD = "idepref_editor_flagPassword"
 	const val WORD_WRAP = "idepref_editor_word_wrap"
 	const val OUTPUT_WORD_WRAP = "idepref_output_word_wrap"
+	const val OUTPUT_LINE_NUMBERS = "idepref_output_line_numbers"
+	const val OUTPUT_TIMESTAMPS = "idepref_output_timestamps"
+	const val OUTPUT_DELTAS = "idepref_output_deltas"
 	const val USE_MAGNIFER = "idepref_editor_use_magnifier"
 	const val USE_ICU = "idepref_editor_useIcu"
 	const val USE_SOFT_TAB = "idepref_editor_useSoftTab"
@@ -133,6 +136,24 @@ object EditorPreferences {
 		get() = prefManager.getBoolean(OUTPUT_WORD_WRAP, true)
 		set(value) {
 			prefManager.putBoolean(OUTPUT_WORD_WRAP, value)
+		}
+
+	var outputLineNumbers: Boolean
+		get() = prefManager.getBoolean(OUTPUT_LINE_NUMBERS, true)
+		set(value) {
+			prefManager.putBoolean(OUTPUT_LINE_NUMBERS, value)
+		}
+
+	var outputTimestamps: Boolean
+		get() = prefManager.getBoolean(OUTPUT_TIMESTAMPS, true)
+		set(value) {
+			prefManager.putBoolean(OUTPUT_TIMESTAMPS, value)
+		}
+
+	var outputDeltas: Boolean
+		get() = prefManager.getBoolean(OUTPUT_DELTAS, true)
+		set(value) {
+			prefManager.putBoolean(OUTPUT_DELTAS, value)
 		}
 
 	var useMagnifier: Boolean
