aab884258f64b0f0889703316a6b429a09df7493
Oluwadara Abijo <oluwadara.abijo@appdevforall.org>
2026-08-13 11:59:39 +0100

    fix(ADFA-4852): Fix dialog dismissal on long-press

--- app/src/main/java/com/itsaky/androidide/utils/DialogExtensions.kt
+++ app/src/main/java/com/itsaky/androidide/utils/DialogExtensions.kt
@@ -5,7 +5,6 @@ import android.app.Activity
 import android.content.Context
 import android.graphics.Rect
 import android.view.MotionEvent
-import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.AdapterView
@@ -16,60 +15,58 @@ import com.itsaky.androidide.idetooltips.TooltipManager
 
 @SuppressLint("ClickableViewAccessibility")
 fun MaterialAlertDialogBuilder.showWithLongPressTooltip(
-    context: Context,
-    tooltipTag: String,
-    vararg customViews: View
+	context: Context,
+	tooltipTag: String,
 ): AlertDialog {
-    val dialog = this.create()
+	val dialog = this.create()
 
-    fun longPressAction() {
-        dialog.dismiss()
-        val anchor = (context as? Activity)?.window?.decorView ?: return
-        TooltipManager.showIdeCategoryTooltip(
-            context = context,
-            anchorView = anchor,
-            tag = tooltipTag,
-        )
-    }
+	fun longPressAction() {
+		val anchor = (context as? Activity)?.window?.decorView ?: return
+		TooltipManager.showIdeCategoryTooltip(
+			context = context,
+			anchorView = anchor,
+			tag = tooltipTag,
+		)
+	}
 
-    dialog.onLongPress {
-        longPressAction()
-        true
-    }
+	dialog.onLongPress {
+		longPressAction()
+		true
+	}
 
-    dialog.listView?.onItemLongClickListener =
-        AdapterView.OnItemLongClickListener { _, _, _, _ ->
-            longPressAction()
-            true
-        }
+	dialog.listView?.onItemLongClickListener =
+		AdapterView.OnItemLongClickListener { _, _, _, _ ->
+			longPressAction()
+			true
+		}
 
-    val customPanel: ViewGroup? = dialog.findViewById(androidx.appcompat.R.id.customPanel)
+	val customPanel: ViewGroup? = dialog.findViewById(androidx.appcompat.R.id.customPanel)
 
-    customPanel?.forEachViewRecursively { view ->
-        if (view is EditText) {
-            dialog.setOnShowListener {
-                view.requestFocus()
-                val imm =
-                    context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
-            }
+	customPanel?.forEachViewRecursively { view ->
+		if (view is EditText) {
+			dialog.setOnShowListener {
+				view.requestFocus()
+				val imm =
+					context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+				imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+			}
 
-            dialog.window?.decorView?.setOnTouchListener { v, event ->
-                if (event.action == MotionEvent.ACTION_DOWN) {
-                    val outRect = Rect()
-                    view.getGlobalVisibleRect(outRect)
-                    if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
-                        view.clearFocus()
-                        val imm =
-                            view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                        imm.hideSoftInputFromWindow(view.windowToken, 0)
-                    }
-                }
-                false
-            }
-        }
-    }
+			dialog.window?.decorView?.setOnTouchListener { v, event ->
+				if (event.action == MotionEvent.ACTION_DOWN) {
+					val outRect = Rect()
+					view.getGlobalVisibleRect(outRect)
+					if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+						view.clearFocus()
+						val imm =
+							view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+						imm.hideSoftInputFromWindow(view.windowToken, 0)
+					}
+				}
+				false
+			}
+		}
+	}
 
-    dialog.show()
-    return dialog
+	dialog.show()
+	return dialog
 }
