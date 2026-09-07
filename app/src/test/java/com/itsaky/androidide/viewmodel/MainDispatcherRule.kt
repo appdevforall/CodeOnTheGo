404f14fd0f774b9971fe01ba3f7a8e17fefe659d
Daniel Alome <astrocoder007@gmail.com>
2026-09-07 20:02:57 +0100

    style: spotless reformat MainDispatcherRule, no functional change

--- app/src/test/java/com/itsaky/androidide/viewmodel/MainDispatcherRule.kt
+++ app/src/test/java/com/itsaky/androidide/viewmodel/MainDispatcherRule.kt
@@ -1,6 +1,5 @@
 package com.itsaky.androidide.viewmodel
 
-
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestDispatcher
@@ -12,14 +11,13 @@ import org.junit.runner.Description
 
 @ExperimentalCoroutinesApi
 class MainDispatcherRule(
-    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+	private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
 ) : TestWatcher() {
+	override fun starting(description: Description) {
+		Dispatchers.setMain(testDispatcher)
+	}
 
-    override fun starting(description: Description) {
-        Dispatchers.setMain(testDispatcher)
-    }
-
-    override fun finished(description: Description) {
-        Dispatchers.resetMain()
-    }
+	override fun finished(description: Description) {
+		Dispatchers.resetMain()
+	}
 }
