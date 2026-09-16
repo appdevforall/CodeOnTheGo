315ee91b809f28179bb020bf00af32f92a72a8b8
itsaky-adfa <akashyadav@appdevforall.org>
2026-09-16 10:05:58 +0530

    ADFA-5421: Read major.minor API versions from api-versions.xml (#2037)

--- subprojects/xml-utils/src/main/java/com/itsaky/androidide/xml/versions/ApiVersionsRegistry.kt
+++ subprojects/xml-utils/src/main/java/com/itsaky/androidide/xml/versions/ApiVersionsRegistry.kt
@@ -30,17 +30,17 @@ import com.itsaky.androidide.xml.registry.XmlRegistry
  * @author Akash Yadav
  */
 interface ApiVersionsRegistry : XmlRegistry<ApiVersions> {
+	companion object {
+		private var sInstance: ApiVersionsRegistry? = null
 
-  companion object {
-
-    private var sInstance: ApiVersionsRegistry? = null
-
-    /** Get the default instance of [ApiVersionsRegistry]. */
-    @JvmStatic
-    fun getInstance(): ApiVersionsRegistry {
-      val klass = ApiVersionsRegistry::class.java
-      return sInstance ?: ServiceLoader.load(klass, klass.classLoader).findFirstOrThrow()
-        .also { sInstance = it }
-    }
-  }
+		/** Get the default instance of [ApiVersionsRegistry]. */
+		@JvmStatic
+		fun getInstance(): ApiVersionsRegistry {
+			val klass = ApiVersionsRegistry::class.java
+			return sInstance ?: ServiceLoader
+				.load(klass, klass.classLoader)
+				.findFirstOrThrow()
+				.also { sInstance = it }
+		}
+	}
 }
