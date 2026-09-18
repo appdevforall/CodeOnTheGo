1b846e8e30c2f04cc69b0a28bbdef2d2df88ee65
Wadamzmail <alksndrmetwakil@gmail.com>
2026-09-18 12:09:37 +0200

    fix(treesitter): synchronize document access

--- editor-treesitter/src/main/java/io/github/rosemoe/sora/editor/ts/TsAnalyzeWorker.kt
+++ editor-treesitter/src/main/java/io/github/rosemoe/sora/editor/ts/TsAnalyzeWorker.kt
@@ -74,6 +74,7 @@ class TsAnalyzeWorker(
   private val messageChannel = LinkedBlockingQueue<Message<*>>()
   private var analyzerJob: Job? = null
   private val lifecycleLock = Any()
+  private val documentLock = Any()
   private var hasStarted = false
   private var resourcesClosed = false
   private var activeDocumentUsers = 0
@@ -172,7 +173,7 @@ class TsAnalyzeWorker(
     }
   }
 
-  internal fun <T> withDocument(block: (TsTextDocument) -> T): T? {
+  fun <T> withDocument(block: (TsTextDocument) -> T): T? {
     synchronized(lifecycleLock) {
       if (resourcesClosed || isDestroyed) {
         return null
@@ -182,7 +183,9 @@ class TsAnalyzeWorker(
     }
 
     try {
-      return block(document)
+      return synchronized(documentLock) {
+        block(document)
+      }
     } finally {
       synchronized(lifecycleLock) {
         activeDocumentUsers--
@@ -284,45 +287,48 @@ class TsAnalyzeWorker(
   }
 
   private fun doInit(init: Init) {
-    document.requestCancellationAndWaitIfParsing()
+    synchronized(documentLock) {
+      document.requestCancellationAndWaitIfParsing()
 
-    check(!isInitialized) {
-      "'Init' must be the first message to TsAnalyzeWorker"
+      check(!isInitialized) {
+        "'Init' must be the first message to TsAnalyzeWorker"
+      }
+
+      document.doInit(init.data)
+      document.reparse()
+      updateStyles()
+
+      isInitialized = true
     }
-
-    document.doInit(init.data)
-    document.reparse()
-    updateStyles()
-
-    isInitialized = true
   }
 
   private fun doMod(mod: Mod) {
+    synchronized(documentLock) {
+      check(isInitialized) {
+        "'Init' must be the first message to TsAnalyzeWorker"
+      }
 
-    check(isInitialized) {
-      "'Init' must be the first message to TsAnalyzeWorker"
+      val textMod = mod.data
+      val edit = textMod.edit
+
+      val oldTree = tree!!
+      oldTree.edit(edit)
+
+      document.doMod(textMod)
+
+      (edit as? TreeSitterInputEdit?)?.recycle()
+
+      document.requestCancellationAndWaitIfParsing()
+
+      if (isDestroyed) {
+        return
+      }
+
+      document.reparse(oldTree)
+
+      oldTree.close()
+      updateStyles()
     }
-
-    val textMod = mod.data
-    val edit = textMod.edit
-
-    val oldTree = tree!!
-    oldTree.edit(edit)
-
-    document.doMod(textMod)
-
-    (edit as? TreeSitterInputEdit?)?.recycle()
-
-    document.requestCancellationAndWaitIfParsing()
-
-    if (isDestroyed) {
-      return
-    }
-
-    document.reparse(oldTree)
-
-    oldTree.close()
-    updateStyles()
   }
 
   private fun updateStyles() {
