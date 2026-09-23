1b798814dd3d4f0e1e6de9a7175454ce97e3423c
Dara Abijo <oluwadara.abijo@appdevforall.org>
2026-09-23 15:09:54 +0100

    feat(ADFA-4398): Init git repo when option is checked

--- app/src/main/java/com/itsaky/androidide/utils/ProjectCreationManager.kt
+++ app/src/main/java/com/itsaky/androidide/utils/ProjectCreationManager.kt
@@ -8,47 +8,63 @@ import com.itsaky.androidide.templates.ProjectTemplateRecipeResult
 import com.itsaky.androidide.templates.StringParameter
 import com.itsaky.androidide.templates.Template
 import com.itsaky.androidide.templates.impl.ConstraintVerifier
+import org.eclipse.jgit.api.Git
 
-class ProjectCreationManager(private val context: Context) {
+class ProjectCreationManager(
+	private val context: Context,
+) {
+	fun execute(
+		template: Template<*>,
+		onStart: () -> Unit,
+		onSuccess: (ProjectTemplateRecipeResult, RecentProject) -> Unit,
+		onError: (String) -> Unit,
+	) {
+		val isValid =
+			template.parameters.filterIsInstance<StringParameter>().all { param ->
+				ConstraintVerifier.isValid(param.value, param.constraints)
+			}
 
-    fun execute(
-        template: Template<*>,
-        onStart: () -> Unit,
-        onSuccess: (ProjectTemplateRecipeResult, RecentProject) -> Unit,
-        onError: (String) -> Unit
-    ) {
-        val isValid = template.parameters.filterIsInstance<StringParameter>().all { param ->
-            ConstraintVerifier.isValid(param.value, param.constraints)
-        }
+		if (!isValid) {
+			onError(context.getString(R.string.msg_invalid_project_details))
+			return
+		}
 
-        if (!isValid) {
-            onError(context.getString(R.string.msg_invalid_project_details))
-            return
-        }
+		onStart()
 
-        onStart()
+		executeAsyncProvideError({
+			val result = template.recipe.execute(TemplateRecipeExecutor(context.applicationContext))
+			if (result is ProjectTemplateRecipeResult && result.data.initGit) {
+				try {
+					Git
+						.init()
+						.setDirectory(result.data.projectDir)
+						.call()
+						.use { }
+				} catch (e: Exception) {
+					e.printStackTrace()
+				}
+			}
+			result
+		}) { result, err ->
+			if (result == null || err != null || result !is ProjectTemplateRecipeResult) {
+				err?.printStackTrace()
+				val errorMsg = err?.cause?.message ?: err?.message ?: context.getString(R.string.project_creation_failed)
+				onError(errorMsg)
+				return@executeAsyncProvideError
+			}
 
-        executeAsyncProvideError({
-            template.recipe.execute(TemplateRecipeExecutor(context.applicationContext))
-        }) { result, err ->
-            if (result == null || err != null || result !is ProjectTemplateRecipeResult) {
-                err?.printStackTrace()
-                val errorMsg = err?.cause?.message ?: err?.message ?: context.getString(R.string.project_creation_failed)
-                onError(errorMsg)
-                return@executeAsyncProvideError
-            }
+			val now = System.currentTimeMillis().toString()
+			val project =
+				RecentProject(
+					location = result.data.projectDir.path,
+					name = result.data.name,
+					createdAt = now,
+					lastModified = now,
+					templateName = template.templateNameStr,
+					language = result.data.language?.name ?: "unknown",
+				)
 
-            val now = System.currentTimeMillis().toString()
-            val project = RecentProject(
-                location = result.data.projectDir.path,
-                name = result.data.name,
-                createdAt = now,
-                lastModified = now,
-                templateName = template.templateNameStr,
-                language = result.data.language?.name ?: "unknown"
-            )
-
-            onSuccess(result, project)
-        }
-    }
+			onSuccess(result, project)
+		}
+	}
 }
